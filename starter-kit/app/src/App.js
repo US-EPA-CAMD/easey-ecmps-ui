c1f4bfd370cad631c071d2cdff37a4695cf335f5
Quang Bui <quangbui@cvpcorp.com>
2020-09-10 11:58:40 -0400

    135 page not found

--- starter-kit/app/src/App.js
+++ starter-kit/app/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import Posts from './components/Posts/Posts';
 import Tasks from './components/Tasks/Tasks';
-
+import NotFound from './components/NotFound/NotFound';
+import Header from './components/Header/Header';
+//import Footer from './components/'
 function App() {
   return (
+<div>
+<Header></Header>
     <Switch>
+      
       <Route path="/posts" component={Posts} />
       <Route path="/tasks" component={Tasks} />      
       <Route path="/" exact component={Home} />
+      <Route path="/home">
+        <Redirect to='/' component={Home}/>
+      </Route>
+      <Route path='*' component={NotFound} />
+
     </Switch>
+    </div>
   );
 }
 
