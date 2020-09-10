c4715620de0f887432aa43d4de77f4047657bd12
Quang Bui <quangbui@cvpcorp.com>
2020-09-10 15:05:00 -0400

    updated app file and sonar cloud

--- starter-kit/app/src/App.js
+++ starter-kit/app/src/App.js
@@ -5,14 +5,12 @@ import Home from './components/Home/Home';
 import Posts from './components/Posts/Posts';
 import Tasks from './components/Tasks/Tasks';
 import NotFound from './components/NotFound/NotFound';
-import Header from './components/Header/Header';
-//import Footer from './components/'
+
 function App() {
   return (
 <div>
-<Header></Header>
+
     <Switch>
-      
       <Route path="/posts" component={Posts} />
       <Route path="/tasks" component={Tasks} />      
       <Route path="/" exact component={Home} />
