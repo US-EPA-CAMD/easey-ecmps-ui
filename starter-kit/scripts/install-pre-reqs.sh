293f8ee6018fba045cae712a15da6556a4a13f84
Srinivasa R Regeti <regeti@regeti.com>
2020-09-14 14:30:06 -0400

    Updated installation instructions

--- starter-kit/scripts/install-pre-reqs.sh
+++ starter-kit/scripts/install-pre-reqs.sh
@@ -3,4 +3,6 @@
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github&version=v6" | tar -zx
 mv cf /usr/local/bin
 sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli-ci/master/ci/installers/completion/cf
+echo $PATH
+ls -l /usr/local/bin
 cf version
