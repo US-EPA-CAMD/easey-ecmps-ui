96ed2c76594412b3e5d18daf1a923aaf8f84c6a7
Srinivasa R Regeti <regeti@regeti.com>
2020-09-23 00:00:40 -0400

    verifying the packaged zip file

--- starter-kit/scripts/package.sh
+++ starter-kit/scripts/package.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "GITHUB_RUN_NUMBER : $GITHUB_RUN_NUMBER"
-echo "GITHUB_WORKSPACE : $GITHUB_WORKSPACE"
-echo "GITHUB_REPOSITORY : $GITHUB_REPOSITORY"
-
 cd $GITHUB_WORKSPACE
 VERSION=$(grep sonar.projectVersion sonar-project.properties | cut -d'=' -f2)
 APP=$(echo $GITHUB_REPOSITORY|cut -d'/' -f2)
@@ -13,3 +9,4 @@ echo "App Name: $APP"
 echo "Build Artifact: $APP.$VERSION.$GITHUB_RUN_NUMBER"
 cd ../
 zip -r $APP.$VERSION.$GITHUB_RUN_NUMBER.zip `basename $GITHUB_WORKSPACE`
+ls -lh $APP.$VERSION.$GITHUB_RUN_NUMBER.zip
