b7ee588f498a47efdab54f3d80b5404a997d5e31
Srinivasa R Regeti <regeti@regeti.com>
2020-09-22 23:41:51 -0400

    Creating the filename with env variables

--- starter-kit/scripts/package.sh
+++ starter-kit/scripts/package.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "$1: GITHUB_RUN_NUMBER"
-echo "$2: GITHUB_WORKSPACE"
-echo "$3: GITHUB_REPOSITORY"
+echo "GITHUB_RUN_NUMBER : $GITHUB_RUN_NUMBER"
+echo "GITHUB_WORKSPACE : $GITHUB_WORKSPACE"
+echo "GITHUB_REPOSITORY : $GITHUB_REPOSITORY"
 
-echo "$GITHUB_RUN_NUMBER : GITHUB_RUN_NUMBER"
-echo "$GITHUB_WORKSPACE : GITHUB_WORKSPACE"
-echo "$GITHUB_REPOSITORY : GITHUB_REPOSITORY"
+cd $GITHUB_WORKSPACE
+VERSION=$(grep sonar.projectVersion sonar-project.properties | cut -d'=' -f2)
+APP=$(echo $GITHUB_REPOSITORY|cut -d'/' -f2)
+
+echo "App version: $VERSION"
+echo "Build Artifact: $APP_$VERSION.$GITHUB_RUN_NUMBER"
+
