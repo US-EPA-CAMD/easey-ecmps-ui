e1b7853b6df206709d75d49e13a4ba8e3719a3c6
Srinivasa Regeti <regeti@regeti.com>
2020-09-23 20:02:07 -0400

    Moved the key retrieval commands from deploy to package

--- starter-kit/scripts/package.sh
+++ starter-kit/scripts/package.sh
@@ -10,4 +10,15 @@ echo "Build Artifact: $APP.$VERSION.$GITHUB_RUN_NUMBER"
 cd ../
 zip -q -x \*.git* -r $APP.$VERSION.$GITHUB_RUN_NUMBER.zip `basename $GITHUB_WORKSPACE`
 ls -lh $APP.$VERSION.$GITHUB_RUN_NUMBER.zip
+
+echo "Retrieving keys ..."
+cf api  https://api.fr.cloud.gov
+cf auth $1 $2
+# S3_CREDENTIALS=`cf service-key "${SERVICE_INSTANCE_NAME}" "${KEY_NAME}" | tail -n +2`
+S3_CREDENTIALS=`cf service-key "deployment-artifacts" "deployment-artifacts-svc-key" | tail -n +2`
+export AWS_ACCESS_KEY_ID=`echo "${S3_CREDENTIALS}" | jq -r .access_key_id`
+export AWS_SECRET_ACCESS_KEY=`echo "${S3_CREDENTIALS}" | jq -r .secret_access_key`
+export BUCKET_NAME=`echo "${S3_CREDENTIALS}" | jq -r .bucket`
+export AWS_DEFAULT_REGION=`echo "${S3_CREDENTIALS}" | jq -r '.region'`
 echo "Region: $AWS_DEFAULT_REGION"
+echo "done..."
