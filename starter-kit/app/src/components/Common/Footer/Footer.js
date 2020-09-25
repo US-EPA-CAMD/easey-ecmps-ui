5b00323997a538ba64e6318f236b66d0058a4c67
Quang Bui <quangbui@cvpcorp.com>
2020-09-25 17:21:30 -0400

    className fix

--- starter-kit/app/src/components/Common/Footer/Footer.js
+++ starter-kit/app/src/components/Common/Footer/Footer.js
@@ -3,16 +3,16 @@ import '../style.css';
 const Footer = () => {
     return (
         <div>
-        <footer class="main-footer clearfix" role="contentinfo">
-		<div class="main-footer__inner">
-			<div class="region-footer">
-				<div class="block-pane-epa-global-footer" id="block-pane-epa-global-footer">
-					<div class="row cols-3">
-						<div class="col size-1of3">
-							<div class="col__title">
+        <footer className="main-footer clearfix" role="contentinfo">
+		<div className="main-footer__inner">
+			<div className="region-footer">
+				<div className="block-pane-epa-global-footer" id="block-pane-epa-global-footer">
+					<div className="row cols-3">
+						<div className="col size-1of3">
+							<div className="col__title">
 								Discover.
 							</div>
-							<ul class="menu">
+							<ul className="menu">
 								<li><a href="https://www.epa.gov/accessibility">Accessibility</a></li>
 								<li><a href="https://www.epa.gov/aboutepa/epas-administrator">EPA Administrator</a></li>
 								<li><a href="https://www.epa.gov/planandbudget">Budget &amp; Performance</a></li>
@@ -23,11 +23,11 @@ const Footer = () => {
 								<li><a href="https://www.epa.gov/privacy">Privacy</a></li>
 							</ul>
 						</div>
-						<div class="col size-1of3">
-							<div class="col__title">
+						<div className="col size-1of3">
+							<div className="col__title">
 								Connect.
 							</div>
-							<ul class="menu">
+							<ul className="menu">
 								<li><a href="https://www.data.gov/">Data.gov</a></li>
 								<li><a href="https://www.epa.gov/office-inspector-general/about-epas-office-inspector-general">Inspector General</a></li>
 								<li><a href="https://www.epa.gov/careers">Jobs</a></li>
@@ -39,25 +39,25 @@ const Footer = () => {
 								<li><a href="https://www.whitehouse.gov/">White House</a></li>
 							</ul>
 						</div>
-						<div class="col size-1of3">
-							<div class="col__title">
+						<div className="col size-1of3">
+							<div className="col__title">
 								Ask.
 							</div>
-							<ul class="menu">
+							<ul className="menu">
 								<li><a href="https://www.epa.gov/home/forms/contact-us">Contact EPA</a></li>
 								<li><a href="https://www.epa.gov/home/epa-hotlines">Hotlines</a></li>
 								<li><a href="https://www.epa.gov/foia">FOIA Requests</a></li>
 								<li><a href="https://www.epa.gov/home/frequent-questions-specific-epa-programstopics">Frequent Questions</a></li>
 							</ul>
-							<div class="col__title">
+							<div className="col__title">
 								Follow.
 							</div>
-							<ul class="social-menu">
-								<li><a class="menu-link social-facebook" href="https://www.facebook.com/EPA">EPA's Facebook</a></li>
-								<li><a class="menu-link social-twitter" href="https://twitter.com/epa">EPA's Twitter</a></li>
-								<li><a class="menu-link social-youtube" href="https://www.youtube.com/user/USEPAgov">EPA's YouTube</a></li>
-								<li><a class="menu-link social-flickr" href="https://www.flickr.com/photos/usepagov">EPA's Flickr</a></li>
-								<li><a class="menu-link social-instagram" href="https://www.instagram.com/epagov">EPA's Instagram</a></li>
+							<ul className="social-menu">
+								<li><a className="menu-link social-facebook" href="https://www.facebook.com/EPA">EPA's Facebook</a></li>
+								<li><a className="menu-link social-twitter" href="https://twitter.com/epa">EPA's Twitter</a></li>
+								<li><a className="menu-link social-youtube" href="https://www.youtube.com/user/USEPAgov">EPA's YouTube</a></li>
+								<li><a className="menu-link social-flickr" href="https://www.flickr.com/photos/usepagov">EPA's Flickr</a></li>
+								<li><a className="menu-link social-instagram" href="https://www.instagram.com/epagov">EPA's Instagram</a></li>
 							</ul>
 
 						</div>
