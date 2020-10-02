ddc2205103177f846f55966ac982f68ab715ed31
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2020-10-02 14:03:07 -0500

    Update build.sh (#296)
    to build facilities api

--- starter-kit/scripts/build.sh
+++ starter-kit/scripts/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+
 echo "Build steps here ... "
 cd starter-kit/app
 npm install
 cd ..
+
 cd starter-kit/facilities-api
 npm install
+npm run build
 cd ..
+
 cd starter-kit/posts-api
 npm install
 cd ..
+
 cd starter-kit/tasks-api
 npm install
 npm run build
