37ccae254fe6ee274edcb37abeccd79b33fb4205
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2020-10-06 11:47:37 -0500

    fixing deployment issues with facilities api (#311)
    Co-authored-by: jasonwhitehead <jasonwhitehad@cvpcorp.com>

--- starter-kit/facilities-api/src/app.module.ts
+++ starter-kit/facilities-api/src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+//import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { FacilitiesModule } from './facilities/facilities.module';
+//import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [FacilitiesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    //TypeOrmModule.forRoot(typeOrmConfig),    
+    FacilitiesModule
+  ],
 })
 export class AppModule {}
