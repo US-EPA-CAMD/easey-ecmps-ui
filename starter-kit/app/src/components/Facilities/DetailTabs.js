8aa82f6198c84a802fc071cbe6add41d4d9ef32d
addisgmichael <68253641+addisgmichael@users.noreply.github.com>
2020-10-26 17:46:24 -0400

    issue#133-added-location

--- starter-kit/app/src/components/Facilities/DetailTabs.js
+++ starter-kit/app/src/components/Facilities/DetailTabs.js
@@ -4,6 +4,7 @@ import Tabs from "../Common/Tabs/Tabs";
 import TabPane from "../Common/Tabs/TabPane";
 import Units from "./Units/Units";
 import MonitoringPlanDataTable from "./MonitoringPlan/MonitoringPlanDataTable";
+import Location from "./Location/Location";
 import * as fs from "../../utils/selectors/facilities";
 
 const DetailTabs = ({ facilities, orisCode }) => {
@@ -21,7 +22,9 @@ const DetailTabs = ({ facilities, orisCode }) => {
         <h4>ORIS Code: {orisCode}</h4>
       </div>
       <Tabs initTab="Location">
-        <TabPane title="Location">Location Content</TabPane>
+        <TabPane title="Location">
+          <Location facility={facility} />
+        </TabPane>
         <TabPane title="Contacts">Contacts Content</TabPane>
         <TabPane title="Units">
           <Units facility={facility} />
