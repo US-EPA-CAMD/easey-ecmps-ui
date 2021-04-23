5b2a0b6795eff406d2e8bc479a05808873fec71c
Tony Davydets <Tony.Davydets@cvp.hcqis.org>
2021-04-23 09:02:45 -0400

    replica 100% complete; side menu changed to "expanded" by default; general clean up

--- src/components/TablePagination/TablePagination.js
+++ src/components/TablePagination/TablePagination.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import "./TablePagination.scss";
-import "./TablePagination.css";
 import { Label } from "@trussworks/react-uswds";
 const TablePagination = ({
   canPreviousPage,
