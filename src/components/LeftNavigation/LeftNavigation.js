bdec513371899fd69ecdf9a611e1e2a738d1c322
Quang Bui <68402491+Qb10@users.noreply.github.com>
2021-06-01 11:47:08 -0400

    Feature/1413 leftnavigation (#1451)
    * 1413
    
    * code smells
    
    * updated styling
    
    * login updates
    
    * code smells

--- src/components/LeftNavigation/LeftNavigation.js
+++ src/components/LeftNavigation/LeftNavigation.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Accessories from "../Accessories/Accessories";
-import Workspace from "../Workspace/Workspace";
-import { Button } from "@trussworks/react-uswds";
+import { Button, SideNav } from "@trussworks/react-uswds";
 import Modal from "../Modal/Modal";
 import Login from "../Login/Login";
 import { NavLink } from "react-router-dom";
@@ -12,8 +11,35 @@ const cdxUser = sessionStorage.getItem("cdx_user")
 const firstName = cdxUser && cdxUser.firstName ? cdxUser.firstName : false;
 
 const LeftNavigation = () => {
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const head = [
+    { name: "Home", url: "/" },
+    { name: "Monitoring Plans", url: "/monitoring-plans" },
+    { name: "QA & Certifications", url: "/qa_certifications" },
+    { name: "Emissions", url: "/emission" },
+  ];
 
+  const workSpace = [
+    { name: "Monitoring Plans", url: "/workspace/monitoring-plans" },
+    { name: "QA & Certifications", url: "/workspace/qa_certifications" },
+    { name: "Emissions", url: "/workspace/emission" },
+  ];
+  const makeHeader = (arr, subFlag) => {
+    return arr.map((item) => {
+      return (
+        <NavLink
+          className={subFlag ? "text-normal" : ""}
+          activeClassName=" usa-current text-primary-dark"
+          to={item.url}
+          exact={true}
+          rel={item.name}
+          title={`Go to ${item.name} page`}
+        >
+          {item.name}
+        </NavLink>
+      );
+    });
+  };
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
   const checkLoggedIn = () => {
     if (cdxUser && firstName) {
       setUserLoggedIn(true);
@@ -35,63 +61,23 @@ const LeftNavigation = () => {
   const openModal = (value) => {
     setShow(value);
   };
-
+  const wsItems = [
+    <NavLink
+      activeClassName=" usa-current text-primary-dark"
+      to="/workspace"
+      rel="workspace"
+      title="Go to the workspace page"
+    >
+      Workspace
+    </NavLink>,
+    [<SideNav items={makeHeader(workSpace, true)} isSubnav />],
+  ];
   return (
-    <div className="bg-base width-full height-full font-body-sm padding-3">
+    <div className="bg-base-lightest width-full height-full font-body-sm padding-3">
       <div className={`usa-overlay ${show ? "is-visible" : ""}`}></div>
-      {/* <Title /> */}
-      <div className="">
-        <div className="grid-row">
-          <NavLink
-            className="text-no-underline text-white"
-            activeClassName=" activeLink"
-            to="/"
-            exact={true}
-            rel="Home"
-            title="Go to the home page"
-          >
-            Home
-          </NavLink>
-        </div>
-        <div className="grid-row">
-          <NavLink
-            className="text-no-underline text-white"
-            to="/monitoring-plans"
-            strict
-            rel="Monitoring Plans"
-            activeClassName=" activeLink"
-            title="Go to the Monitoring Plans page"
-          >
-            Monitoring Plans
-          </NavLink>
-        </div>
-        <div className="grid-row">
-          <NavLink
-            className="text-no-underline text-white"
-            to="/qa_certifications"
-            strict
-            activeClassName=" activeLink"
-            rel={"QA & Certifications"}
-            title={"Go to the QA & Certifications page"}
-          >
-            {" QA & Certifications"}
-          </NavLink>
-        </div>
+      <SideNav items={makeHeader(head)} />
 
-        <div className="grid-row">
-          <NavLink
-            className="text-no-underline text-white"
-            activeClassName=" activeLink"
-            strict
-            to="/emissions"
-            rel="Emissions"
-            title="Go to the Emissions page"
-          >
-            Emissions
-          </NavLink>
-        </div>
-      </div>
-      {userLoggedIn ? <Workspace /> : ""}
+      {userLoggedIn ? <SideNav items={wsItems} /> : ""}
       <div className="padding-bottom-4 position-absolute bottom-0">
         {!cdxUser ? (
           <Button onClick={() => openModal(true)}> Log In</Button>
@@ -102,7 +88,9 @@ const LeftNavigation = () => {
         <Accessories />
       </div>
       {show ? (
-        <Modal show={show} close={closeModalHandler} children={<Login />} />
+        <div className="">
+          <Modal show={show} close={closeModalHandler} children={<Login />} />
+        </div>
       ) : (
         ""
       )}
