318420d235427024ba0895faf3357bd9354541d7
Quang Bui <68402491+Qb10@users.noreply.github.com>
2021-06-03 18:16:19 -0400

    Feature/1290 tab (#1476)
    * tab order
    
    * spelling
    
    * conflict

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,48 +1,50 @@
 import React, { useEffect, useState } from "react";
-import './Tabs.scss';
-const Tabs = ({ children, dynamic = false, removeTabs, setResizeObserver, setActive}) => {
+import "./Tabs.scss";
+const Tabs = ({
+  children,
+  dynamic = false,
+  removeTabs,
+  setResizeObserver,
+  setActive,
+}) => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
-  const settingActiveTab =(index) =>{
+  const settingActiveTab = (index) => {
     setActiveTabIndex(index);
-    setActive(false,index-1);
-
-  }
-  let contentBox=null;
+    setActive(false, index - 1);
+  };
+  let contentBox = null;
   const closeHandler = (event, index) => {
     event.stopPropagation();
     removeTabs(index);
     if (activeTabIndex === children.length - 1) {
       setActiveTabIndex(index - 1);
-      setActive(false,index-2);
+      setActive(false, index - 2);
     }
   };
 
-  useEffect(()=>{
-    if(contentBox){
+  useEffect(() => {
+    if (contentBox) {
       setResizeObserver(contentBox);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[contentBox]);
+  }, [contentBox]);
 
   return (
     <div>
       <div className="">
-      <ul className="usa-button-group usa-button-group--segmented" >
-        {children.map((el, i) => (
-          <li key={i} className="usa-button-group__item usa-tooltip" style={{position:'relative'}} data-position="bottom" title={el.props.title}>
-            <button
-            tabIndex={0}
-              className={
-                activeTabIndex === i
-                  ? "active-button button-group"
-                  : "notActive-button button-group"
-              }
-              onClick={() => settingActiveTab(i)}
+        <ul className="usa-button-group usa-button-group--segmented">
+          {children.map((el, i) => (
+            <li
+              key={i}
+              className="usa-button-group__item usa-tooltip"
+              style={{ position: "relative" }}
+              data-position="bottom"
+              title={el.props.title}
             >
               {dynamic ? (
                 <i
-                aria-label={`close ${el.props.title} tab`}
-                tabIndex={0}
+                  aria-label={`close ${el.props.title} tab`}
+                  tabIndex={0}
                   className={
                     i === 0
                       ? "fa fa-times close-icon hidden"
@@ -51,20 +53,35 @@ const Tabs = ({ children, dynamic = false, removeTabs, setResizeObserver, setAct
                   onClick={(e) => closeHandler(e, i)}
                 />
               ) : null}
-              <span className={i!==0?"tabTitle":"firstTabTitle"}>{el.props.title}</span>
-            </button>
-          </li>
-        ))}
-      </ul>
+              <button
+                tabIndex={0}
+                aria-label={`open ${el.props.title} tab`}
+                className={
+                  activeTabIndex === i
+                    ? "active-button button-group"
+                    : "notActive-button button-group"
+                }
+                onClick={() => settingActiveTab(i)}
+              >
+                <span className={i !== 0 ? "tabTitle" : "firstTabTitle"}>
+                  {el.props.title}
+                </span>
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
       <div
         className="tabContent"
-        ref={el => {
+        ref={(el) => {
           if (!el) {
-            return
+            return;
           }
-          contentBox=el
-        }}>{children[activeTabIndex]}</div>
+          contentBox = el;
+        }}
+      >
+        {children[activeTabIndex]}
+      </div>
     </div>
   );
 };
