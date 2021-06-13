d605531f37a41bb8b35e047d3cfc2409fcfa8c7e
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2021-06-12 23:47:35 -0500

    fixing manifest & env variables for inactivity timer

--- src/config.js
+++ src/config.js
@@ -10,12 +10,12 @@ const title = "EPA Easey";
 export const config = {
   app: {
     activityEvents,
+    inactivityDuration:
+      process.env.REACT_APP_EASEY_UI_INACTIVITY_DURATION_MINUTES * oneMinute || oneMinute,
     activityPollingFrequency:
       process.env.REACT_APP_EASEY_UI_ACTIVITY_POLLING_FREQUENCY_SECONDS * oneSecond || oneSecond,
     countdownDuration:
       process.env.REACT_APP_EASEY_UI_ACTIVITY_COUNTDOWN_DURATION_SECONDS * oneSecond || 30 * oneSecond,
-    inactivityDuration:
-      process.env.REACT_APP_EASEY_UI_INACTIVITY_DURATION_MINUTES * oneMinute || oneMinute,
     env: process.env.REACT_APP_EASEY_UI_PORTAL_ENV || "local-dev",
       published: process.env.REACT_APP_EASEY_UI_PORTAL_PUBLISHED || "local",
     version: process.env.REACT_APP_EASEY_UI_PORTAL_VERSION || "v0.0.0",
