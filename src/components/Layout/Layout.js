00e5db47099025763b5d3019a1151cf037adb70b
tonydavydets <tonydavdyets@cvpcorp.com>
2021-07-19 18:34:20 -0400

    responsiveness fixes in progress

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -25,7 +25,7 @@ const Layout = (props) => {
       <LeftNavToSubHeader />
 
       <div className="grid-row">
-        <div className="grid-col-2 bg-base-lightest mobile:display-none desktop:display-block">
+        <div className="grid-col-2 bg-base-lightest mobile-lg:display-none desktop:display-none desktop-lg:display-block">
           <LeftNavigation user={props.user} logOut={props.logOut} />
         </div>
         <div className="grid-col margin-x-2 minh-tablet-lg" id="main">
