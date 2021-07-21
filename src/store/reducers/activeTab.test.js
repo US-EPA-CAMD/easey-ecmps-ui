0cceb19c439d893e036928a3e8ed6aa862d3e7b5
Quang Bui <quangbui@cvpcorp.com>
2021-07-21 12:02:07 -0400

    apistatusreducer test

--- src/store/reducers/activeTab.test.js
+++ src/store/reducers/activeTab.test.js
@@ -1,29 +1,22 @@
 import activeTabReducer from "../reducers/activeTab";
 import * as actionsTypes from "../actions/activeTab";
 
-
 describe("setting active tab ", () => {
   it("should update state to be 1 ", () => {
-
-
-    const setActiveTab = actionsTypes.setActiveTab(1,1);
-    const active = activeTabReducer( [0],setActiveTab);
+    const setActiveTab = actionsTypes.setActiveTab(1, 1);
+    const active = activeTabReducer([0], setActiveTab);
     expect(active[0]).toBe(1);
   });
 
   it("testing no state passed in ", () => {
-
-
-    const setActiveTab = actionsTypes.setActiveTab(1,1);
-    const active = activeTabReducer( null,setActiveTab);
+    const setActiveTab = actionsTypes.setActiveTab(1, 1);
+    const active = activeTabReducer(null, setActiveTab);
     expect(active[1]).not.toBeDefined();
   });
 
   it("testing not active_tab type passed in  ", () => {
-
-
-    const setActiveTab = actionsTypes.setActiveTab(1,1);
-    const active = activeTabReducer( null,{type:''});
+    const setActiveTab = actionsTypes.setActiveTab(1, 1);
+    const active = activeTabReducer(null, { type: "" });
     expect(active[1]).not.toBeDefined();
   });
 });
--- src/store/reducers/apiStatusReducer.js
+++ src/store/reducers/apiStatusReducer.js
@@ -1,52 +1,54 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 
-const reducer = (state = initialState.apiCallsInProgress, action) => {
+const reducer = (state, action) => {
+  
+  let currentState = state ? state : initialState.apiCallsInProgress;
   let returnObject;
 
   switch (action.type) {
     case types.BEGIN_FACILITIES_API_CALL:
-      returnObject = Object.assign({}, state, { facilities: true });
+      returnObject = Object.assign({}, currentState, { facilities: true });
       break;
     case types.LOAD_FACILITIES_SUCCESS:
-      returnObject = Object.assign({}, state, { facilities: false });
+      returnObject = Object.assign({}, currentState, { facilities: false });
       break;
     case types.BEGIN_MONITORING_PLANS_API_CALL:
-      returnObject = Object.assign({}, state, { monitoringPlans: true });
+      returnObject = Object.assign({}, currentState, { monitoringPlans: true });
       break;
     case types.LOAD_MONITORING_PLANS_SUCCESS:
-      returnObject = Object.assign({}, state, { monitoringPlans: false });
+      returnObject = Object.assign({}, currentState, { monitoringPlans: false });
       break;
     case types.BEGIN_MONITORING_METHODS_API_CALL:
-      returnObject = Object.assign({}, state, { monitoringMethods: true });
+      returnObject = Object.assign({}, currentState, { monitoringMethods: true });
       break;
     case types.LOAD_MONITORING_METHODS_SUCCESS:
-      returnObject = Object.assign({}, state, { monitoringMethods: false });
+      returnObject = Object.assign({}, currentState, { monitoringMethods: false });
       break;
     case types.BEGIN_MONITORING_MATSMETHODS_API_CALL:
-      returnObject = Object.assign({}, state, { monitoringMatsMethods: true });
+      returnObject = Object.assign({}, currentState, { monitoringMatsMethods: true });
       break;
     case types.LOAD_MONITORING_MATSMETHODS_SUCCESS:
-      returnObject = Object.assign({}, state, { monitoringMatsMethods: false });
+      returnObject = Object.assign({}, currentState, { monitoringMatsMethods: false });
       break;
     case types.BEGIN_MONITORING_SYSTEMS_API_CALL:
-      returnObject = Object.assign({}, state, { monitoringSystems: true });
+      returnObject = Object.assign({}, currentState, { monitoringSystems: true });
       break;
     case types.LOAD_MONITORING_SYSTEMS_SUCCESS:
-      returnObject = Object.assign({}, state, { monitoringSystems: false });
+      returnObject = Object.assign({}, currentState, { monitoringSystems: false });
       break;
     case types.BEGIN_MONITORING_SYSTEMS_COMPONENTS_API_CALL:
-      returnObject = Object.assign({}, state, {
+      returnObject = Object.assign({}, currentState, {
         monitoringSystemsComponents: true,
       });
       break;
     case types.LOAD_MONITORING_SYSTEMS_COMPONENTS_SUCCESS:
-      returnObject = Object.assign({}, state, {
+      returnObject = Object.assign({}, currentState, {
         monitoringSystemsComponents: false,
       });
       break;
     default:
-      returnObject = state;
+      returnObject = currentState;
       break;
   }
 
