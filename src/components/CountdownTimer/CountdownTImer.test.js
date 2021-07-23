f6a39a9c7659f4138c736524d8d3e2e81ebb709e
Quang Bui <quangbui@cvpcorp.com>
2021-07-22 22:06:14 -0400

    countonw timer test

---
+++ src/components/CountdownTimer/CountdownTImer.test.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { CountdownTimer, CountdownTimerRender } from "./CountdownTimer";
+import { mount, ReactWrapper, shallow } from "enzyme";
+
+describe("67440874", () => {
+  let wrapper: ReactWrapper;
+
+  //   afterEach(() => {
+  //     jest.restoreAllMocks();
+  //   });
+  test("...", () => {
+    const useRefSpy = jest
+      .spyOn(React, "useRef")
+      .mockReturnValueOnce({ current: 0 });
+    wrapper = shallow(<CountdownTimer duration={0} countdownAPI={jest.fn()} />);
+    let time = wrapper.find(".time-wrapper");
+
+    expect(time).toBeDefined();
+  });
+
+  test("...", () => {
+    const useRefSpy = jest
+      .spyOn(React, "useRef")
+      .mockReturnValueOnce({ current: 1000 });
+    wrapper = mount(
+      <CountdownTimerRender remainingTime={0} countdownAPI={jest.fn()} />
+    );
+    let time = wrapper.find(".time-wrapper");
+    expect(time).toBeDefined();
+  });
+  test("...", () => {
+    const useRefSpy = jest
+      .spyOn(React, "useRef")
+      .mockReturnValueOnce({ current: 0 });
+    wrapper = mount(
+      <CountdownTimerRender remainingTime={0} countdownAPI={jest.fn()} />
+    );
+    let time = wrapper.find(".time-wrapper");
+    expect(time).toBeDefined();
+  });
+
+  test("...", () => {
+    const useRefSpy = jest
+      .spyOn(React, "useRef")
+      .mockReturnValueOnce({ current: 0 });
+    wrapper = mount(
+      <CountdownTimerRender remainingTime={1000} countdownAPI={jest.fn()} />
+    );
+    let time = wrapper.find(".time-wrapper");
+    expect(time).toBeDefined();
+  });
+  test("...", () => {
+    const useRefSpy = jest
+      .spyOn(React, "useRef")
+      .mockReturnValueOnce({ current: null });
+    wrapper = mount(
+      <CountdownTimerRender remainingTime={1000} countdownAPI={jest.fn()} />
+    );
+    let time = wrapper.find(".time-wrapper");
+    expect(time).toBeDefined();
+  });
+
+  test("...", () => {
+    const useRefSpy = jest
+      .spyOn(React, "useRef")
+      .mockReturnValueOnce({ current: 2000 });
+    wrapper = mount(
+      <CountdownTimerRender remainingTime={2000} countdownAPI={jest.fn()} />
+    );
+    let time = wrapper.find(".time-wrapper");
+    expect(time).toBeDefined();
+  });
+
+  test("...", () => {
+    const useRefSpy = jest
+      .spyOn(React, "useRef")
+      .mockReturnValueOnce({ current: null });
+    wrapper = mount(
+      <CountdownTimerRender remainingTime={0} countdownAPI={jest.fn()} />
+    );
+    let time = wrapper.find(".time-wrapper");
+    expect(time).toBeDefined();
+  });
+});
