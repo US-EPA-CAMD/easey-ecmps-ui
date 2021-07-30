93d6d318e2790974b7ffe6a352c4b8aa32d67299
tonydavydets <tonydavdyets@cvpcorp.com>
2021-07-30 15:24:59 -0400

    everything but links

--- src/components/datatablesContainer/SelectFacilitiesDataTable/SelectFacilitiesDataTable.js
+++ src/components/datatablesContainer/SelectFacilitiesDataTable/SelectFacilitiesDataTable.js
@@ -36,7 +36,7 @@ export const SelectFacilitiesDataTable = ({
 
   useEffect(() => {
     obtainCheckedOutLocations().then();
-  }, []);
+  }, [openedFacilityTabs]);
 
   /*useInterval(() => {
     obtainCheckedOutLocations().then();
