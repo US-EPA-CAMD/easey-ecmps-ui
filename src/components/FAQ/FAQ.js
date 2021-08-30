ce73239229067e1db3867906ea3b3a5583c9a684
tonydavydets <tonydavdyets@cvpcorp.com>
2021-08-30 10:50:22 -0400

    FAQ page complete

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { Accordion, AccordionItem } from "@trussworks/react-uswds";
-import NotFound from "../NotFound/NotFound";
+import { Accordion } from "@trussworks/react-uswds";
+
 const FAQ = () => {
   const topics = [
     {
       name: "Monitoring Plans",
       questions: [
         {
-          question: "what?",
-          answer: "yes",
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
         },
         {
-          question: "what?",
-          answer: "yes",
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
         },
         {
-          question: "what?",
-          answer: "yes",
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
         },
       ],
     },
@@ -24,8 +24,12 @@ const FAQ = () => {
       name: "QA & Certifications",
       questions: [
         {
-          question: "what?",
-          answer: "yes",
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
+        },
+        {
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
         },
       ],
     },
@@ -33,54 +37,61 @@ const FAQ = () => {
       name: "Emissions",
       questions: [
         {
-          question: "what?",
-          answer: "yes",
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
+        },
+        {
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
+        },
+        {
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
+        },
+        {
+          question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+          answer: "Phasellus tincidunt velit sed leo porttitor tincidunt",
         },
       ],
     },
   ];
 
   const createAccordionItems = (questions) => {
-    return questions.map((question) => {
-      return (
-        <Accordion
-          items={[
-            <AccordionItem
-              id={`${question.question.split(" ").join("")}`}
-              title={question.question}
-              content={<NotFound />} 
-              expanded = {true}
-            />,
-          ]}
-          bordered={false}
-          multiselectable={true}
-         
-        />
-      );
+    // *** create items
+    let items = [];
+    let keyCount = 1;
+
+    questions.forEach((element) => {
+      items.push({
+        title: element.question,
+        content: element.answer,
+        expanded: false,
+        id: `question_${keyCount++}`,
+      });
     });
+
+    return <Accordion items={items} bordered={false} multiselectable={true} />;
   };
+
   return (
     <div className="grid-row padding-top-7 padding-2 " id="faqPage">
       <div className="grid-col-9 fit-content">
         <div>
           <span className="text-bold font-heading-2xl">FAQs</span>
           <p>
-            {" "}
-            Examine she brother prudent add day ham. Far stairs now coming bed
-            oppose hunted become his. You zealously departure had procuring
-            suspicion. Examine she brother prudent add day ham. Far stairs now
-            coming bed oppose hunted become his. You zealously departure had
-            procuring suspicion. Books whose front would purse if be do decay.
-            Quitting you way formerly diBooks who{" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis
+            arcu in ipsum sollicitudin viverra. Vestibulum ut tincidunt lacus.
+            Aliquam erat volutpat. Donec eget tincidunt mauris. Sed ac orci a
+            risus vehicula molestie quis sit amet ligula. Sed faucibus, neque
+            eget finibus lobortis, nunc erat molestie diam, sed pulvinar metus
           </p>
         </div>
-
         {topics.map((topic) => {
           return (
             <div className=" padding-top-2 padding-bottom-2">
               {" "}
               <h2 className="text-bold font-heading-xl">{topic.name} </h2>
-             {createAccordionItems(topic.questions)}
+              {createAccordionItems(topic.questions)}
             </div>
           );
         })}{" "}
