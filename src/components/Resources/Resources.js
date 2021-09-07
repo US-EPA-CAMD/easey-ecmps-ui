339deb994eed22501f8b8bc102e12bfd7ac24d79
tonydavydets <tonydavdyets@cvpcorp.com>
2021-09-07 13:49:53 -0400

    Resources pages implemented

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { Link as USWDSLink } from "@trussworks/react-uswds";
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+import { Link as USWDSLink, Button } from "@trussworks/react-uswds";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
 import { Link } from "react-router-dom";
-import './Resources.scss'
-const Resources = ({ user, setCurrentLink }) => {
+import "./Resources.scss";
+
+const Resources = () => {
   const topics = [
     {
       name: "Glossary",
       descriptions:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla massa in lectus volutpat scelerisque. Cras eu leo vel lacus tincidunt molestie. Vestibulum faucibus enim sit amet pretium laoreet.",
       url: "/monitoring-plans",
     },
     {
       name: "Reporting Instructions",
       descriptions:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla massa in lectus volutpat scelerisque. Cras eu leo vel lacus tincidunt molestie. Vestibulum faucibus enim sit amet pretium laoreet.",
       url: "/qa_certifications",
     },
     {
       name: "CAM API",
       descriptions:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla massa in lectus volutpat scelerisque. Cras eu leo vel lacus tincidunt molestie. Vestibulum faucibus enim sit amet pretium laoreet.",
       url: "/emission",
     },
   ];
@@ -36,33 +37,31 @@ const Resources = ({ user, setCurrentLink }) => {
     },
   ];
 
-  // handled current left bar vertical blue bar in subHeader component
-  // const handleRouteChange = (event, url) => {
-  //   setCurrentLink(false);
-  // };
-
   return (
     <div className="grid-row padding-top-7 padding-2 react-transition fade-in">
       <div className="grid-col-auto fit-content">
         <div>
           <span className="text-bold font-heading-2xl">Resources</span>
           <p>
-            {" "}
-            Examine she brother prudent add day ham. Far stairs now coming bed
-            oppose hunted become his. You zealously departure had procuring
-            suspicion. Books who{" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            fringilla massa in lectus volutpat scelerisque. Cras eu leo vel
+            lacus tincidunt molestie. Vestibulum faucibus enim sit amet pretium
+            laoreet.
           </p>
         </div>
         <div className="grid-row">
           {topics.map((topic) => {
             return (
-              <div className=" padding-top-2 padding-bottom-2 grid-col-4 padding-right-1">
+              <div
+                className=" padding-top-2 padding-bottom-2 grid-col-4 padding-right-1"
+                key={`container-${topic.name.replace(/ /g, "-")}`}
+              >
                 {" "}
                 <h2 className="text-bold font-heading-xl">{topic.name} </h2>
                 <div>{topic.descriptions}</div>
                 <USWDSLink
-                  className="usa-button usa-button--outline"
-                  outline
+                  className="usa-button usa-button--outline margin-top-1 margin-left-05"
+                  outline="true"
                   type="button"
                   variant="unstyled"
                   asCustom={Link}
@@ -75,7 +74,7 @@ const Resources = ({ user, setCurrentLink }) => {
                   id={`${topic.name.split(" ").join("")}`}
                   // onClick={(event) => handleRouteChange(event, topic.url)}
                 >
-                  View{" "}
+                  Visit{" "}
                   {topic.name === "Glossary"
                     ? `the ${topic.name}`
                     : `${topic.name}`}
@@ -97,25 +96,25 @@ const Resources = ({ user, setCurrentLink }) => {
         </div>
 
         {/* <ul> */}
-          {additionalResources.map((ele) => {
-            return (
-              <li>
-                <u>
-                  <Link
-                    className="linkColor"
-                    href={ele.url}
-                    role="link"
-                    rel={ele.name}
-                    title={`Go to ${ele.name} page`}
-                    key={ele.url}
-                    id={`${ele.name.split(" ").join("")}`}
-                  >
-                    {ele.name} <OpenInNewIcon/>
-                  </Link>
-                </u>
-              </li>
-            );
-          })}
+        {additionalResources.map((ele) => {
+          return (
+            <li key={`li_${ele.name.replace(/ /g, "")}`}>
+              <Button
+                type="button"
+                unstyled={true}
+                className="text-primary text-underline"
+                href={ele.url}
+                role="link"
+                rel={ele.name}
+                title={`Go to ${ele.name} page`}
+                key={ele.url}
+                id={`${ele.name.split(" ").join("")}`}
+              >
+                {ele.name} <OpenInNewIcon />
+              </Button>
+            </li>
+          );
+        })}
         {/* </ul> */}
       </div>
     </div>
