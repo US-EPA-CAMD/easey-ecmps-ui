4dd3337c6562f4a9d5470f574169b4875d1731f4
Dion Olympia <dionolympia@cvpcorp.com>
2021-11-04 13:15:09 -0400

    unit tests for methods selector

--- src/utils/selectors/monitoringPlanMethods.js
+++ src/utils/selectors/monitoringPlanMethods.js
@@ -30,7 +30,7 @@ const formatStringToDate = (date) => {
   return `${parts[1]}/${parts[2]}/${parts[0]}`;
 };
 
-const formatAndNormalizeDate = (date) => {
+export const formatAndNormalizeDate = (date) => {
   const parts = date.split("-");
 
   return `${parts[1]}/${parts[2].substring(0, 2)}/${parts[0]}`;
@@ -52,78 +52,6 @@ export const getMonitoringPlansMatsMethodsTableRecords = (data) => {
       col3: `${beginDate} ${beginHour}`,
       col4: `${endDate} ${endHour}`,
       col5: el.id,
-    });
-  });
-
-  return records;
-};
-
-export const getMonitoringPlansFuelDataRecords = (data) => {
-  const records = [];
-
-  data.forEach((el) => {
-    const beginDate = el.beginDate
-      ? formatAndNormalizeDate(el.beginDate.toString())
-      : "";
-
-    const endDate = el.endDate
-      ? formatAndNormalizeDate(el.endDate.toString())
-      : "";
-
-    records.push({
-      col1: el["fuelCode"],
-      col2: el["indicatorCode"],
-      col3:
-        el["ozoneSeasonIndicator"] === null
-          ? null
-          : el["ozoneSeasonIndicator"] === 0
-          ? "Yes"
-          : "No",
-      col4: el["demGCV"],
-      col5: el["demSO2"],
-      col6: `${beginDate}`,
-      col7: `${endDate}`,
-      col8: el.id,
-    });
-  });
-
-  return records;
-};
-
-export const getMonitoringPlansUnitControlRecords = (data) => {
-  const records = [];
-
-  data.forEach((el) => {
-    const optimizationDate = el["optimizationDate"]
-      ? formatAndNormalizeDate(el["optimizationDate"].toString())
-      : "";
-
-    const installDate = el["installDate"]
-      ? formatAndNormalizeDate(el["installDate"].toString())
-      : "";
-    const retireDate = el["retireDate"]
-      ? formatAndNormalizeDate(el["retireDate"].toString())
-      : "";
-
-    records.push({
-      col1: el["parameterCode"],
-      col2: el["controlCode"],
-      col3:
-        el["originalCode"] === null
-          ? null
-          : el["originalCode"] === 0
-          ? "Yes"
-          : "No",
-      col4: `${optimizationDate}`,
-      col5: `${installDate}`,
-      col6:
-        el["seasonalControlsIndicator"] === null
-          ? null
-          : el["seasonalControlsIndicator"] === 0
-          ? "Yes"
-          : "No",
-      col7: `${retireDate}`,
-      col8: el.id,
     });
   });
 
