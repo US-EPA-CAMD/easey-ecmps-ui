7580248cc202e22f8ba6bc53df95088f72e76874
Quang Bui <68402491+Qb10@users.noreply.github.com>
2021-11-04 15:01:31 -0400

    Feature/1722 view capacity (#158)
    * test
    
    * view updated

--- src/utils/selectors/monitoringPlanUnitCapacity.js
+++ src/utils/selectors/monitoringPlanUnitCapacity.js
@@ -7,14 +7,21 @@ export const getMonitoringPlansUnitCapacityRecords = (data) => {
     const beginHour = el.beginHour ? el.beginHour.toString() : "";
     const endDate = el.endDate ? formatStringToDate(el.endDate.toString()) : "";
     const endHour = el.endHour ? el.endHour.toString() : "";
+
+
+    const commercialOperationDateString = el.commercialOperationDate ? formatStringToDate(el.commercialOperationDate.toString()) : "";
+    const operationDateString = el.operationDate ? formatStringToDate(el.operationDate.toString()) : "";
+    const boilerTurbineBeginDateString = el.boilerTurbineBeginDate ? formatStringToDate(el.boilerTurbineBeginDate.toString()) : "";
+    const boilerTurbineEndDateString = el.boilerTurbineEnd ? formatStringToDate(el.boilerTurbineEnd.toString()) : "";
+
     records.push({
-      col1: "", //el.maximumHourlyHeatInputCapacity,
+      col1: commercialOperationDateString,
 
-      col2: "", //`${beginDate} ${beginHour}`,
-      col3: "", //`${endDate} ${endHour}`,
-      col4: "", //el.id,
+      col2: operationDateString,
+      col3: el.boilerTurbineType,
+      col4: boilerTurbineBeginDateString,
 
-      col5: "", //el.id,
+      col5: boilerTurbineEndDateString,
       col6: el.maximumHourlyHeatInputCapacity,
 
       col7: `${beginDate} ${beginHour}`,
