008a313aa19dbbb5329784089e06aba1a3aa532e
Dion Olympia <dionolympia@cvpcorp.com>
2021-11-06 23:15:34 -0400

    added unit test for pct quals

--- src/utils/selectors/monitoringPlanPCTQualifications.test.js
+++ src/utils/selectors/monitoringPlanPCTQualifications.test.js
@@ -7,73 +7,41 @@ describe("testing monitoring plan data selectors", () => {
   beforeAll(() => {
     selectedMonitoringPCTQualifications = [
       {
-        id: "CAMD-543ABF5EB72647E5A693F99BF24D2FB9",
-        locationId: "60",
-        qualificationTypeCode: "PK",
-        beginDate: "2006-01-01T00:00:00.000Z",
-        endDate: null,
-        userId: "PQA09Q1",
-        addDate: "2009-02-20",
+        id: "DPGLISSO9-635AF13C866142E6A5CF72782D274618",
+        qualificationId: "DPGLISSO9-EB4EDE87B8294FBC86FED070BA25E9E8",
+        qualificationYear: "2013",
+        averagePercentValue: "0.9",
+        yr1QualificationDataYear: "2010",
+        yr1QualificationDataTypeCode: "A",
+        yr1PercentageValue: "1.1",
+        yr2QualificationDataYear: "2011",
+        yr2QualificationDataTypeCode: "A",
+        yr2PercentageValue: "1.6",
+        yr3QualificationDataYear: "2012",
+        yr3QualificationDataTypeCode: "A",
+        yr3PercentageValue: "0.0",
+        userId: "phh",
+        addDate: "2013-04-16",
         updateDate: null,
-        active: true,
-        leeQualifications: null,
-        lmeQualifications: null,
-        pctQualifications: null,
-      },
-      {
-        id: "EP-CONTROL-3AC36B6D87C34A01A07097FC611F020C",
-        locationId: "60",
-        qualificationTypeCode: "GF",
-        beginDate: null,
-        endDate: "2009-01-01T00:00:00.000Z",
-        userId: "RHamerni",
-        addDate: "2009-09-21",
-        updateDate: null,
-        active: true,
-        leeQualifications: null,
-        lmeQualifications: null,
-        pctQualifications: null,
-      },
-      {
-        id: "EP-CONTROL-F9384183CDDF4104AEDEB1855D375ECD",
-        locationId: "60",
-        qualificationTypeCode: "PK",
-        beginDate: "2000-01-01T00:00:00.000Z",
-        endDate: "2005-12-31T00:00:00.000Z",
-        userId: "RHamerni",
-        addDate: "2009-09-21",
-        updateDate: null,
-        active: false,
-        leeQualifications: null,
-        lmeQualifications: null,
-        pctQualifications: null,
       },
     ];
 
     monitoringPCTQualificationRecords = [
       {
-        col1: "PK",
-        col2: "01/01/2006",
-        col3: "",
-        col4: "CAMD-543ABF5EB72647E5A693F99BF24D2FB9",
-      },
-      {
-        col1: "GF",
-        col2: "",
-        col3: "01/01/2009",
-        col4: "EP-CONTROL-3AC36B6D87C34A01A07097FC611F020C",
-      },
-      {
-        col1: "PK",
-        col2: "01/01/2000",
-        col3: "12/31/2005",
-        col4: "EP-CONTROL-F9384183CDDF4104AEDEB1855D375ECD",
+        col1: "2013",
+        col2: "0.9",
+        col3: "2010",
+        col4: "2011",
+        col5: "2012",
+        col6: "DPGLISSO9-635AF13C866142E6A5CF72782D274618",
       },
     ];
   });
   test("should generate data table records for monitoring formulas", () => {
     expect(
-      fs.getMonitoringPlansQualifications(selectedMonitoringPCTQualifications)
+      fs.getMonitoringPlansPCTQualifications(
+        selectedMonitoringPCTQualifications
+      )
     ).toEqual(monitoringPCTQualificationRecords);
   });
 });
