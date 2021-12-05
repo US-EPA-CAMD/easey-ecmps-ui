c91008f493ad5c486a03badd970ebac189187f61
Quang Bui <quangbui@cvpcorp.com>
2021-12-04 19:59:10 -0500

    updated test files

--- src/additional-functions/add-active-class.test.js
+++ src/additional-functions/add-active-class.test.js
@@ -1,6 +1,11 @@
-import { addActiveClass,removeActiveClass,handleActiveElementFocus } from "./add-active-class";
+import { addActiveClass,handleActiveElementFocus } from "./add-active-class";
 
 describe("testing checkout function", () => {
-  expect(addActiveClass(null)).toBeTrue()
-  
+
+  test("called addActiveClass", () => {
+  const mockedFormEvent = { target: { querySelectorAll: jest.fn() }, preventDefault: jest.fn() };
+  expect(addActiveClass(mockedFormEvent)).toEqual(1)
+  expect(handleActiveElementFocus(mockedFormEvent)).toEqual(1)
+
+  });
 });
