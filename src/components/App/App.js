1f60d1859cb812057ad3e03aaea7523f6505b6a2
Conrad Sanley <csanley@cvpcorp.com>
2022-02-07 16:46:55 -0500

    Addressed code smells

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -107,7 +107,7 @@ const App = () => {
 
             <Route path={`/faqs`} exact component={() => <FAQ />} />
             <Route path="/login" exact component={Login} />
-            {user ? <Redirect from="/monitoring-plans" to="/workspace/monitoring-plans" /> 
+            {user ? <Redirect from="/monitoring-plans" to="/workspace/monitoring-plans" />
             : <Redirect from="/workspace/monitoring-plans" to="/monitoring-plans" />}
             <Route
               path="/monitoring-plans"
@@ -127,7 +127,7 @@ const App = () => {
                 />
               )}
             />
-            {user ? <Redirect from="/qa_certifications" to="/workspace/qa_certifications" /> 
+            {user ? <Redirect from="/qa_certifications" to="/workspace/qa_certifications" />
             : <Redirect from="/workspace/qa_certifications" to="/qa_certifications" />}
             <Route path="/qa_certifications/" exact component={ComingSoon} />
             <Route
@@ -135,7 +135,7 @@ const App = () => {
               exact
               component={ComingSoon}
             />
-            {user ? <Redirect from="/emission" to="/workspace/emission" /> 
+            {user ? <Redirect from="/emission" to="/workspace/emission" />
             : <Redirect from="/workspace/emission" to="/emission" />}
             <Route path="/emission/" exact component={ComingSoon} />
             <Route path="/workspace/emission/" exact component={ComingSoon} />
