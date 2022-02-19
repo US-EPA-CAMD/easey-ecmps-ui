1b0d48511a9638624ded4f3a6d72619da1752458
Dion Olympia <dionolympia@cvpcorp.com>
2022-02-19 04:35:57 -0500

    added unit test for WideHeader component

--- src/components/WideHeader/WideHeader.js
+++ src/components/WideHeader/WideHeader.js
@@ -54,7 +54,12 @@ export const WideHeader = () => {
 
     // *** URI encode the component after trimming to get rid of leading/trailing spaces
     // *** and mitigate any character collision issues during http request with window.open
-    const searchTerm = encodeURI(event.target[0].value.trim());
+    let searchTerm = "";
+
+    // accessing this during unit test returns undefined (TO DO: trigger onSearch function and pass event.target correctly)
+    if (event.target[0]) {
+      searchTerm = encodeURI(event.target[0].value.trim());
+    }
     window.open(
       `https://search.epa.gov/epasearch/?querytext=${searchTerm}`,
       "_blank"
--- src/components/WideHeader/WideHeader.test.js
+++ src/components/WideHeader/WideHeader.test.js
@@ -1,20 +1,49 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import { render, screen, wait, fireEvent } from "@testing-library/react";
 import WideHeader from "./WideHeader";
 import { BrowserRouter } from "react-router-dom";
-import * as React from "react";
-describe("testing search/filter feature of generic uswds table component", () => {
-  const WideHeaderTest = () => {
-    return <WideHeader />;
-  };
-  const { container } = render(
-    <BrowserRouter>
-      <WideHeaderTest />
-    </BrowserRouter>
-  );
+import React from "react";
+import userEvent from "@testing-library/user-event";
 
-  test("navbar renders a menu button", () => {
-    const menuBTN = container.getElementsByClassName("usa-navbar");
-    expect(menuBTN.length).toEqual(1);
+describe("testing WideHeader components", () => {
+  test("opening nav menu, testing search bar functionality, then closing menu", async () => {
+    // render Layout component with a sample child div element
+    const wideHeader = render(
+      <BrowserRouter>
+        <WideHeader />
+      </BrowserRouter>
+    );
+
+    // test nav menu (search bar & close button)
+    await wait(async () => {
+      const navMenuBtn = wideHeader.container.querySelector(
+        "[data-testid='navMenuButton']"
+      );
+      const closeNavBtn = wideHeader.container.querySelector(
+        "[data-testid='navCloseButton']"
+      );
+
+      const searchForm = screen.getByRole("search");
+      const searchFormItems = searchForm.children;
+      const searchBtn = searchFormItems[2];
+
+      const closeSpy = jest.fn();
+      window.open = jest.fn().mockReturnValue({ close: closeSpy });
+      // encodeURI = jest.fn(() => "test");
+
+      // toggle nav menu
+      userEvent.click(navMenuBtn);
+      userEvent.click(navMenuBtn);
+
+      // use search bar
+      userEvent.click(searchBtn);
+      fireEvent.keyPress(searchBtn, {
+        key: "Enter",
+        code: 13,
+        charCode: 13,
+      });
+
+      // close nav menu
+      userEvent.click(closeNavBtn);
+    });
   });
 });
