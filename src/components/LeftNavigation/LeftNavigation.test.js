a453f1bb7c992473ad8cbac142536a2c8c045c63
Quang Bui <quangbui@cvpcorp.com>
2022-04-08 17:23:09 -0400

    left hand menu for q and a

--- src/components/LeftNavigation/LeftNavigation.test.js
+++ src/components/LeftNavigation/LeftNavigation.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Layout from "../Layout/Layout";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import LeftNavigation from "./LeftNavigation";
 
 describe("Left Navigation links", () => {
   test("renders links correctly", () => {
-    render(
+    const { container } = render(
       <BrowserRouter>
         <Layout>
           <Switch>
@@ -14,17 +14,17 @@ describe("Left Navigation links", () => {
               path="/"
               exact
               component={() => (
-                <LeftNavigation
-                  user={true}
-                />
+                <LeftNavigation user={true} setCurrentLink={jest.fn()} />
               )}
             />
           </Switch>
         </Layout>
       </BrowserRouter>
-    )
-  
+    );
+
     const homeLink = screen.getAllByText("Home");
+    const homeBtn = container.querySelector("#Home");
+    fireEvent.click(homeLink[homeLink.length - 1]);
     expect(homeLink).not.toBeUndefined();
 
     const wksLink = screen.getAllByText("Workspace");
@@ -37,7 +37,6 @@ describe("Left Navigation links", () => {
     expect(qaLink).not.toBeUndefined();
 
     const emLink = screen.getAllByText("Emissions");
-    expect(emLink).not.toBeUndefined(); 
-
+    expect(emLink).not.toBeUndefined();
   });
 });
