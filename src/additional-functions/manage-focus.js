012c5b4ad18eef44ad10a7b099a7db53a702750c
Quang Bui <68402491+Qb10@users.noreply.github.com>
2022-04-22 13:19:27 -0400

    Feature/3433 refresh bug (#390)
    * fixed prefiltering
    
    * added float / decimal validations to ui
    
    * fixed last focus element bug
    
    * added comments
    
    * code smells

--- src/additional-functions/manage-focus.js
+++ src/additional-functions/manage-focus.js
@@ -75,16 +75,21 @@ export const returnFocusToLast = () => {
       window["lastFocusedArray"][window["lastFocusedArray"].length - 1];
 
     console.log("found: ", lastFocus);
-
+    let counter = 0;
     if (lastFocus && lastFocus.id) {
       const selectedFocus = document.querySelector(`#${lastFocus.id}`);
       console.log("set", selectedFocus);
 
-      while (document.activeElement !== selectedFocus) {
+      // counter is trivial number, can be 1.
+      while (document.activeElement !== selectedFocus && counter < 5) {
         selectedFocus
-          ? console.log('selectedFocus.focus()',selectedFocus.focus())
-          : console.log("did not find lastfocus id element");
-        console.log("testing while");
+          ? console.log("selectedFocus.focus()", selectedFocus.focus())
+          : // might need to check for an edge case when the array is empty
+            console.log(
+              "did not find lastfocus id element",
+              window["lastFocusedArray"][0].focus()
+            );
+        counter++;
       }
     }
   }
