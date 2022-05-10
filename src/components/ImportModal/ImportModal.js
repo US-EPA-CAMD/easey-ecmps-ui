eb8c86ae9edcf9cf49bcccdda6153d35fa6eefef
Quang Bui <68402491+Qb10@users.noreply.github.com>
2022-05-10 12:45:19 -0400

    Feature/revert button fix (#403)
    * update dtos
    
    * updated left nav
    
    * updated workspace menu with new workspace title
    
    * import schema validation
    
    * import validation with api call
    
    * removed errors
    
    * fixed conflicts
    
    * fixed coloring for nav
    
    * fixed revert button

--- src/components/ImportModal/ImportModal.js
+++ src/components/ImportModal/ImportModal.js
@@ -102,7 +102,7 @@ const ImportModal = ({
           <div
             className="padding-right-2 padding-left-3 "
             aria-live="polite"
-          >
+          > { console.log('imported error',importedFileErrorMsgs)}
             {importedFileErrorMsgs.map((error, i) => (
               <Alert type="error" slim noIcon key={i} role="alert">
                 {error}
