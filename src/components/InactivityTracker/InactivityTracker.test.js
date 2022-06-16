e1470d937d779da276d6a9d61f10a0b5aae20d2a
Quang Bui <quangbui@cvpcorp.com>
2022-06-16 11:34:39 -0400

    unit testing

--- src/components/InactivityTracker/InactivityTracker.test.js
+++ src/components/InactivityTracker/InactivityTracker.test.js
@@ -97,7 +97,104 @@
 //     expect(logOut).toHaveBeenCalled();
 //   });
 // });
-test("test file", () => {
-  const val = 1;
-  expect(val === 1);
+// import React from "react";
+// import { config } from "../../config";
+// import { render, unmountComponentAtNode } from "react-dom";
+// import { act } from "react-dom/test-utils";
+// import { InactivityTracker } from "./InactivityTracker";
+// import { logOut } from "../../utils/api/easeyAuthApi";
+// import { checkoutAPI } from "../../additional-functions/checkout";
+import React from "react";
+import { render,fireEvent } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
+import {
+  InactivityTracker,
+  mapStateToProps,
+  mapDispatchToProps,
+} from "./InactivityTracker";
+import { Provider } from "react-redux";
+import configureStore from "../../store/configureStore.dev";
+const store = configureStore();
+const axios = require("axios");
+jest.mock("axios");
+
+const props = {
+  openedFacilityTabs: [{ checkout: true }],
+  setCheckout: jest.fn(),
+};
+
+const noCheckoutProps = {
+  openedFacilityTabs: [{ checkout: false }],
+  setCheckout: jest.fn(),
+};
+describe("InactivityTracker", () => {
+  let callback = jest.fn();
+  beforeEach(() => {
+    // we're using fake timers because we don't want to
+    // wait a full second for this test to run.
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    callback.mockRestore();
+    jest.clearAllTimers();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test("tests InactivityTracker", async () => {
+    const { container } = render(
+      <Provider store={store}>
+        <InactivityTracker {...props} />{" "}
+      </Provider>
+    );
+    jest.advanceTimersByTime(3000);
+    expect(container).not.toBeUndefined();
+  });
+
+  test("tests InactivityTracker with no checkout tabs", async () => {
+    const { container } = render(
+      <Provider store={store}>
+        <InactivityTracker {...noCheckoutProps} />{" "}
+      </Provider>
+    );
+    jest.advanceTimersByTime(3000);
+
+    
+    const clickDiv = container.querySelector("div");
+    console.log('div',clickDiv);
+    fireEvent.click(clickDiv)
+    expect(container).not.toBeUndefined();
+  });
+
+  test("tests InactivityTracker with no checkout tabs and not active", async () => {
+    const { container } = render(
+      <Provider store={store}>
+        <InactivityTracker {...noCheckoutProps} />{" "}
+      </Provider>
+    );
+
+    jest.advanceTimersByTime(100000000);
+    expect(container).not.toBeUndefined();
+  });
+  test("mapDispatchToProps calls the appropriate action", async () => {
+    // mock the 'dispatch' object
+    const dispatch = jest.fn();
+    const actionProps = mapDispatchToProps(dispatch);
+    const state = store.getState();
+    const stateProps = mapStateToProps(state);
+
+    const formData = [];
+    // verify the appropriate action was called
+    actionProps.setCheckout();
+    expect(state).toBeDefined();
+  });
+
+  // test("should init hook with delay", () => {
+  //   const { result } = renderHook(() => InactivityTracker({...props}));
+
+  //   expect(result.current).toBeUndefined();
+  //   expect(setInterval).toHaveBeenCalledTimes(1);
+  //   expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 5000);
+  // });
 });
