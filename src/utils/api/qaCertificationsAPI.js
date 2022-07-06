f4367d9b55ce7ca3f05788996ee30f3bdef8ac23
Quang Bui <68402491+Qb10@users.noreply.github.com>
2022-07-06 10:22:11 -0400

    Revert "Feature/3702 export data (#453)"
    This reverts commit d91ff16a6b1c3e6cb0a21dacd54a15607e20535a.

--- src/utils/api/qaCertificationsAPI.js
+++ src/utils/api/qaCertificationsAPI.js
@@ -72,17 +72,33 @@ export const exportQA = async (
   endDate
 ) => {
   let url = `${config.services.qaCertification.uri}/export?facilityId=${facilityId}`;
-  if (unitIds?.length > 0) {
-    const unitIdsQueryParam = unitIds.join("|");
-    url = `${url}&unitIds=${unitIdsQueryParam}`;
+
+  if (unitIds && unitIds.length > 0) {
+    let unitIdsString = "";
+    unitIds.forEach((e, idx) => {
+      if (idx !== 0 && idx < unitIds.length) {
+        unitIdsString += "|";
+      }
+      unitIdsString += e;
+    });
+    url = `${url}&unitIds=${unitIdsString}`;
   }
-  if (stackPipeIds?.length > 0) {
-    const stackPipeIdsQueryParam = stackPipeIds.join("|");
-    url = `${url}&stackPipeIds=${stackPipeIdsQueryParam}`;
+
+  if (stackPipeIds && stackPipeIds.length > 0) {
+    let stackPipeIdsString = "";
+    stackPipeIds.forEach((e, idx) => {
+      if (idx !== 0 && idx < unitIds.length) {
+        stackPipeIdsString += "|";
+      }
+      stackPipeIdsString += e;
+    });
+    url = `${url}&stackPipeIds=${stackPipeIdsString}`;
   }
+
   if (beginDate && endDate) {
     url = `${url}&beginDate=${beginDate}&endDate=${endDate}`;
   }
+
   try {
     return handleResponse(
       await secureAxios({
