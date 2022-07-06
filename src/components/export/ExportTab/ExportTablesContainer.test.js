c764d13d0306425e4a3953db6ddb327df2b79c6c
Corey W <wencorey0@gmail.com>
2022-07-06 16:59:10 -0400

    ExportTablesContainer unit test

---
+++ src/components/export/ExportTab/ExportTablesContainer.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ExportTablesContainer from "./ExportTablesContainer";
+import * as qaCertificationsAPI from "../../../utils/api/qaCertificationsAPI";
+
+const props = {
+  selectionData: { beginDate: '1/1/11', endDate: '1/1/11' },
+  selectedConfig: { locations: [] },
+  exportState: {},
+  setExportState: null,
+  workspaceSection: 'workspacesection',
+  orisCode: 'orisCode',
+  dataRef: {},
+}
+
+test.only('renders QA Test Summary table title', async () => {
+  const response = { data: { testSummaryData: [] } }
+  qaCertificationsAPI.exportQA = jest.fn().mockReturnValue(response)
+  render(<ExportTablesContainer {...props} />);
+  const testSummaryTitle = await screen.findByText(/test summary/i)
+  expect(testSummaryTitle).toBeInTheDocument();
+})
