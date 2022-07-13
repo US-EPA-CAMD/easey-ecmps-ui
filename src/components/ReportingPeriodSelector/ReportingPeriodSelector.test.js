16dc43c630dea7ce9a3bc410a0cda3e9aa096988
Corey W <106772898+cw-cvp@users.noreply.github.com>
2022-07-13 16:46:56 -0400

    mock axios instead of getReportingPeriod

--- src/components/ReportingPeriodSelector/ReportingPeriodSelector.test.js
+++ src/components/ReportingPeriodSelector/ReportingPeriodSelector.test.js
@@ -3,7 +3,9 @@ import { render, screen, waitForElement, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import ReportingPeriodSelector from "./ReportingPeriodSelector"
-import * as qaCertificationsAPI from "../../utils/api/qaCertificationsAPI";
+
+const axios = require("axios");
+jest.mock("axios");
 
 const props = {
   getInitSelection: jest.fn(),
@@ -11,52 +13,50 @@ const props = {
   reportingPeriodSelectionHandler: jest.fn(),
 }
 
-const response = {
-  data: [
-    {
-      "id": 1,
-      "calendarYear": 1993,
-      "quarter": 1,
-      "beginDate": "1993-01-01",
-      "endDate": "1993-03-31",
-      "periodDescription": "1993 QTR 1",
-      "periodAbbreviation": "1993 Q1",
-      "archiveInd": 0,
-      "selected": false
-    },
-    {
-      "id": 5,
-      "calendarYear": 1994,
-      "quarter": 1,
-      "beginDate": "1994-01-01",
-      "endDate": "1994-03-31",
-      "periodDescription": "1994 QTR 1",
-      "periodAbbreviation": "1994 Q1",
-      "archiveInd": 0,
-      "selected": false
-    },
-    {
-      "id": 6,
-      "calendarYear": 1994,
-      "quarter": 2,
-      "beginDate": "1994-04-01",
-      "endDate": "1994-06-30",
-      "periodDescription": "1994 QTR 2",
-      "periodAbbreviation": "1994 Q2",
-      "archiveInd": 0,
-      "selected": false
-    },
-  ]
-}
+const reportingPeriods = [
+  {
+    "id": 1,
+    "calendarYear": 1993,
+    "quarter": 1,
+    "beginDate": "1993-01-01",
+    "endDate": "1993-03-31",
+    "periodDescription": "1993 QTR 1",
+    "periodAbbreviation": "1993 Q1",
+    "archiveInd": 0,
+    "selected": false
+  },
+  {
+    "id": 5,
+    "calendarYear": 1994,
+    "quarter": 1,
+    "beginDate": "1994-01-01",
+    "endDate": "1994-03-31",
+    "periodDescription": "1994 QTR 1",
+    "periodAbbreviation": "1994 Q1",
+    "archiveInd": 0,
+    "selected": false
+  },
+  {
+    "id": 6,
+    "calendarYear": 1994,
+    "quarter": 2,
+    "beginDate": "1994-04-01",
+    "endDate": "1994-06-30",
+    "periodDescription": "1994 QTR 2",
+    "periodAbbreviation": "1994 Q2",
+    "archiveInd": 0,
+    "selected": false
+  },
+]
 
 const yearLabelText = /year/i
 const quarterLabelText = /quarter/i
 
+axios.get.mockResolvedValue({ status: 200, data: reportingPeriods })
+
 test('renders year/quarter/reporting period labels', async () => {
   // Arrange
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
   render(<ReportingPeriodSelector {...props} />)
-
   const yearLabel = await screen.findByLabelText(/year/i)
   const quarterLabel = screen.getByLabelText(/quarter/i)
   const rpLabel = screen.getByLabelText(/reporting period/i)
@@ -69,10 +69,8 @@ test('renders year/quarter/reporting period labels', async () => {
 
 test('renders correct number of options for year dropdown', async () => {
   // Arrange
-  const numYearOptions = new Set(response.data.map(rp => rp.calendarYear))
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
+  const numYearOptions = new Set(reportingPeriods.map(rp => rp.calendarYear))
   await waitForElement(() => render(<ReportingPeriodSelector {...props} />))
-
   const yearDropdown = screen.getByLabelText(yearLabelText)
   const yearOptions = within(yearDropdown).getAllByRole('option')
 
@@ -82,11 +80,9 @@ test('renders correct number of options for year dropdown', async () => {
 
 test('renders correct number of options for quarter dropdown', async () => {
   // Arrange
-  const singleYearData = response.data.filter(rp => rp.calendarYear === 1994)
+  const singleYearData = reportingPeriods.filter(rp => rp.calendarYear === 1994)
   const numQuarterOptions = new Set(singleYearData.map(rp => rp.quarter))
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
   await waitForElement(() => render(<ReportingPeriodSelector {...props} />))
-
   const quarterDropdown = screen.getByLabelText(quarterLabelText)
   const quarterOptions = within(quarterDropdown).getAllByRole('option')
 
@@ -96,7 +92,6 @@ test('renders correct number of options for quarter dropdown', async () => {
 
 test('when a user selects a year from the dropdown then that element\'s selected value is truthy', async () => {
   // Arrange
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
   await waitForElement(() => render(<ReportingPeriodSelector {...props} />))
   const yearDropdown = screen.getByLabelText(yearLabelText)
   const selectedOption = screen.getByRole('option', { name: /1993/i })
@@ -113,7 +108,6 @@ test('when a user selects a year from the dropdown then that element\'s selected
 
 test('when a user selects a quarter from the dropdown then that element\'s selected value is truthy', async () => {
   // Arrange
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
   await waitForElement(() => render(<ReportingPeriodSelector {...props} />))
   const selectedOption = screen.getByRole('option', { name: /Q1/i })
   const unselectedOption = screen.getByRole('option', { name: /Q2/i })
@@ -123,7 +117,6 @@ test('when a user selects a quarter from the dropdown then that element\'s selec
   // select earlier quarter, quarter option defaults to most recent quarter
   userEvent.selectOptions(quarterDropdown, '1');
 
-
   // Assert
   expect(selectedOption.selected).toBeTruthy()
   expect(unselectedOption.selected).toBeFalsy();
@@ -132,15 +125,13 @@ test('when a user selects a quarter from the dropdown then that element\'s selec
 test('when a user selects a year from the dropdown then begin/end date reflect that change', async () => {
   // Arrange
   const selectedYear = '1993'
-  const selectedDataObj = response.data.find(reportingPeriod => reportingPeriod.calendarYear === parseInt(selectedYear))
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
+  const selectedDataObj = reportingPeriods.find(reportingPeriod => reportingPeriod.calendarYear === parseInt(selectedYear))
   await waitForElement(() => render(<ReportingPeriodSelector {...props} />))
   const yearDropdown = screen.getByLabelText(yearLabelText)
 
   // Act
   // select a previous year, init selection is most recent year/quarter
   userEvent.selectOptions(yearDropdown, selectedYear);
-
 
   const beginDate = screen.getByText(selectedDataObj.beginDate)
   const endDate = screen.getByText(selectedDataObj.endDate)
@@ -152,17 +143,15 @@ test('when a user selects a year from the dropdown then begin/end date reflect t
 
 test('when a user selects a quarter from the dropdown then begin/end date reflect that change', async () => {
   // Arrange
-  const dataForSingleYear = response.data.filter(rp => rp.calendarYear === 1994)
+  const dataForSingleYear = reportingPeriods.filter(rp => rp.calendarYear === 1994)
   const selectedQuarter = '1'
   const selectedDataObj = dataForSingleYear.find(reportingPeriod => reportingPeriod.quarter === parseInt(selectedQuarter))
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
   await waitForElement(() => render(<ReportingPeriodSelector {...props} />))
   const quarterDropdown = screen.getByLabelText(quarterLabelText)
 
   // Act
   // select a previous quarter, init selection is most recent year/quarter
   userEvent.selectOptions(quarterDropdown, selectedQuarter);
-
 
   const beginDate = screen.getByText(selectedDataObj.beginDate)
   const endDate = screen.getByText(selectedDataObj.endDate)
@@ -175,7 +164,6 @@ test('when a user selects a quarter from the dropdown then begin/end date reflec
 test('given an export state with reportingPeriodId exists then year/quarter dropdowns init with that reporting period selected', async () => {
   // Arrange
   const exportState = { reportingPeriodId: 1 } // selected year: 1993, quarter: Q1
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
   await waitForElement(() => render(<ReportingPeriodSelector {...{ ...props, exportState }} />))
   const yearDropdown = screen.getByLabelText(yearLabelText)
   const quarterDropdown = screen.getByLabelText(quarterLabelText)
@@ -192,7 +180,6 @@ test('when only monitoring plan is checked then dropdowns are disabled', async (
   const mpName = 'monitoring-plan'
   const dataTypes = [{ label: "Monitoring Plan", name: mpName, checked: true, }]
   const isExport = true;
-  qaCertificationsAPI.getReportingPeriod = jest.fn().mockReturnValueOnce(response)
   await waitForElement(() => render(<ReportingPeriodSelector {...{ ...props, isExport, dataTypes }} />))
   const yearDropdown = screen.getByLabelText(yearLabelText)
   const quarterDropdown = screen.getByLabelText(quarterLabelText)
