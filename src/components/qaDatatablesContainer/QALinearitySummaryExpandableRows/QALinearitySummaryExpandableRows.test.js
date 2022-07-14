764c784fcc0fde769e5f62ef4651eaeb769ba99f
Corey W <106772898+cw-cvp@users.noreply.github.com>
2022-07-14 17:19:46 -0400

    add tests to check remove

--- src/components/qaDatatablesContainer/QALinearitySummaryExpandableRows/QALinearitySummaryExpandableRows.test.js
+++ src/components/qaDatatablesContainer/QALinearitySummaryExpandableRows/QALinearitySummaryExpandableRows.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render, waitForElement, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import QALinearitySummaryExpandableRows from "./QALinearitySummaryExpandableRows";
 
 import * as qaApi from "../../../utils/api/qaCertificationsAPI";
@@ -7,11 +9,30 @@ const axios = require("axios");
 
 jest.mock("axios");
 
+const rowToRemoveText = 'this row should be removed'
+
 const linearitySummary = [
   {
     "id": "IT07D0112-4CBC1A08D61B403DBB179D0B78EC51A8",
     "testSumId": "IT07D0112-70AA39C4632746999222EC8FB3C530FB",
     "gasLevelCode": "MID",
+    "meanMeasuredValue": 55.3,
+    "calculatedMeanMeasuredValue": 55.3,
+    "meanReferenceValue": 55.9,
+    "calculatedMeanReferenceValue": 55.9,
+    "percentError": 1.1,
+    "calculatedPercentError": 1.1,
+    "apsIndicator": 0,
+    "calculatedAPSIndicator": 0,
+    "userId": "lperez",
+    "addDate": "4/25/2011, 7:30:54 PM",
+    "updateDate": null,
+    "linearityInjectionData": []
+  },
+  {
+    "id": "idToRemove",
+    "testSumId": "IT07D0112-70AA39C4632746999222EC8FB3C530FB",
+    "gasLevelCode": rowToRemoveText,
     "meanMeasuredValue": 55.3,
     "calculatedMeanMeasuredValue": 55.3,
     "meanReferenceValue": 55.9,
@@ -48,10 +69,12 @@ const linearitySummary = [
 const componentRenderer = (locId, testSummaryId) => {
   const props = {
     user: { firstName: "test" },
-    data :{
+    data: {
       locationId: locId,
       id: testSummaryId
-    }
+    },
+    actionsBtn: 'View',
+    actionColumnName: 'Linearity Summary Data"'
   };
   return render(<QALinearitySummaryExpandableRows {...props} />);
 };
@@ -64,9 +87,41 @@ test("testing linearity summary expandable records from test summary data", asyn
   expect(res.data).toEqual(linearitySummary);
   let { container } = await waitForElement(() => componentRenderer("5930", "IT07D0112-70AA39C4632746999222EC8FB3C530FB"));
   expect(container).toBeDefined();
-  //screen.debug();
   expect(screen.getByRole("table")).toBeDefined();
   expect(screen.getAllByRole("columnheader").length).toBe(6);
-  expect(screen.getAllByRole("row").length).toBe(linearitySummary.length+1);
+  expect(screen.getAllByRole("row").length).toBe(linearitySummary.length + 1);
+});
+
+test.only("when remove button on a row is clicked then that row is deleted from the table", async () => {
+  // Arrange
+  axios.get.mockImplementation(() => Promise.resolve({ status: 200, data: linearitySummary }));
+  // works with this mock
+  qaApi.deleteQALinearitySummary = jest.fn()
+  // fails and encounters error with this mock
+  // axios.delete.mockImplementation(() => Promise.resolve({ status: 200, data: 'delete succeeded' }))
+  const rowIndex = 1
+  const res = await qaApi.getQALinearitySummary("5930", "IT07D0112-70AA39C4632746999222EC8FB3C530FB");
+  expect(res.data).toEqual(linearitySummary);
+  await waitForElement(() => componentRenderer("5930", "IT07D0112-70AA39C4632746999222EC8FB3C530FB"));
+
+  const removeButtons = screen.getAllByRole('button', { name: /remove/i })
+
+  // row exists before remove
+  expect(screen.queryByText(rowToRemoveText)).toBeInTheDocument()
+
+  // Act
+  // click remove button in second row
+  const secondRemoveButton = removeButtons[rowIndex]
+  userEvent.click(secondRemoveButton)
+
+  // click 'yes' in confirmation modal
+  const confirmBtns = screen.getAllByRole('button', { name: /yes/i })
+  const secondConfirmBtn = confirmBtns[rowIndex]
+  userEvent.click(secondConfirmBtn)
+
+  const removedRowText = screen.queryByText(rowToRemoveText)
+
+  // Assert
+  expect(removedRowText).not.toBeInTheDocument()
 });
 
