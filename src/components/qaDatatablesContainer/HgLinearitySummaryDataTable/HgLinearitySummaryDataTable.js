1f33b34b426b2c773360d3bac7f12d38a0859faf
Corey W <106772898+cw-cvp@users.noreply.github.com>
2022-07-26 12:13:38 -0400

    add datatable

--- src/components/qaDatatablesContainer/HgLinearitySummaryDataTable/HgLinearitySummaryDataTable.js
+++ src/components/qaDatatablesContainer/HgLinearitySummaryDataTable/HgLinearitySummaryDataTable.js
@@ -1,11 +1,54 @@
+import { Button } from "@trussworks/react-uswds";
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 import { loadDropdowns } from "../../../store/actions/dropdowns";
+import { getQATestSummaryByCode } from "../../../utils/api/qaCertificationsAPI";
+import QADataTableRender from "../../QADataTableRender/QADataTableRender";
 
-export const HgLinearitySummaryDataTable = () => {
+export const HgLinearitySummaryDataTable = ({
+  locationSelectValue
+}) => {
+
+  console.log('location select value', locationSelectValue);
+
+  const testFetch = async () => {
+    const codes = ['HGSI3', 'HGLINE']
+    // const codes = ['HGLINE']
+    const resp = await getQATestSummaryByCode(locationSelectValue.id, { testTypeCodes: codes })
+    console.log('resp', resp);
+  }
+
+  const columns = [
+    "Test Type Code",
+    "Unit or Stack Pipe ID",
+    "Component ID",
+    "Test Number",
+    "Test Reason Code",
+    "Test Result Code",
+    "End Date",
+    "End Hour",
+    "End Minute",
+  ];
+
+  const data = []
+
   return (
-    <h1>hg linearity temp</h1>
+    <>
+      <h1>hg linearity temp</h1>
+      <Button onClick={testFetch}>test fetch</Button>
+      <QADataTableRender
+        columnNames={columns}
+        columnWidth={10}
+        data={data}
+        // openHandler={openModal}
+        // onRemoveHandler={onRemoveHandler}
+        actionColumnName={"Actions"}
+        actionsBtn={"View"}
+        // user={user}
+        // expandableRowComp={<QALinearitySummaryExpandableRows user={user} />}
+      />
+    </>
   )
 }
 
