2db4f1c7a0aa8938000a885cd68761d80b7b1223
Corey W <106772898+cw-cvp@users.noreply.github.com>
2022-07-28 11:35:59 -0400

    ignore files for testing

--- src/components/qaDatatablesContainer/HgLinearitySummaryDataTable/HgLinearitySummaryDataTable.js
+++ src/components/qaDatatablesContainer/HgLinearitySummaryDataTable/HgLinearitySummaryDataTable.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import React from "react";
 
 import { getTestSummary } from "../../../utils/selectors/QACert/TestSummary";
--- src/components/qaDatatablesContainer/MiscDatatables/MiscDataTables.js
+++ src/components/qaDatatablesContainer/MiscDatatables/MiscDataTables.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import React from "react";
 
 import TestSummaryDataTable from "../TestSummaryDataTable/TestSummaryDataTable";
