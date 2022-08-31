928138a018b965718879738e99c0ffd7aa6f6d71
Corey W <106772898+cw-cvp@users.noreply.github.com>
2022-08-31 13:40:59 -0400

    remove unused tests

--- src/components/qaDatatablesContainer/QALinearitySummaryDataTable/QALinearitySummaryDataTable.test.js
+++ src/components/qaDatatablesContainer/QALinearitySummaryDataTable/QALinearitySummaryDataTable.test.js
@@ -484,60 +484,6 @@ function componentRendererNoData(args) {
   return render(<QALinearitySummaryDataTable {...props} />);
 }
 
-test("tests a test summary", async () => {
-  expect(true).toBe(true);
-  // axios.get.mockImplementation(() =>
-  //   Promise.resolve({ status: 200, data: testSummary })
-  // );
-  // const title = await qaApi.getQATestSummary(3);
-  // expect(title.data).toEqual(testSummary);
-  // let { container } = await waitForElement(() => componentRenderer(3));
-  // expect(container).toBeDefined();
-});
-
-test("tests a test summary NO USER/NO DATA", async () => {
-  expect(true).toBe(true);
-  // axios.get.mockImplementation(() =>
-  //   Promise.resolve({ status: 200, data: [] })
-  // );
-  // const title = await qaApi.getQATestSummary(3);
-  // expect(title.data).toEqual([]);
-  // let { container } = await waitForElement(() => componentRendererNoData(3));
-  // expect(container).toBeDefined();
-});
-
-test("tests updating test summary data", async () => {
-  expect(true).toBe(true);
-  // axios.get.mockResolvedValue({ status: 200, data: testSummary });
-  // const payload = {
-  //   stackPipeId: testSummary.stackPipeId,
-  //   unitId: testSummary.unitId,
-  //   testTypeCode: testSummary.testTypeCode,
-  //   componentID: testSummary.componentID,
-  //   spanScaleCode: testSummary.spanScaleCode,
-  //   testNumber: "A05-Q4-2011-20",
-  //   testReasonCode: testSummary.testReasonCode,
-  //   testResultCode: testSummary.testResultCode,
-  //   beginDate: testSummary.beginDate,
-  //   beginHour: testSummary.beginHour,
-  //   beginMinute: testSummary.beginMinute,
-  //   endDate: testSummary.endDate,
-  //   endHour: testSummary.endHour,
-  //   endMinute: testSummary.endMinute,
-  //   gracePeriodIndicator: testSummary.gracePeriodIndicator,
-  //   testComment: "dev testing",
-  // };
-  // axios.mockResolvedValueOnce(Promise.resolve({ status: 200, data: payload }));
-  // const title = await qaApi.updateQALinearityTestSummary(
-  //   1873,
-  //   "f6e4056f-c779-4a61-b265-2932898a9033",
-  //   payload
-  // );
-  // expect(title.data).toEqual(payload);
-  // let { container } = await waitForElement(() => componentRenderer(1873));
-  // expect(container).toBeDefined();
-});
-
 test('renders QALinearitySummaryDataTable', () => {
   // Arrange
   const { container } = render(<Provider store={store}><QALinearitySummaryDataTable {...props} /></Provider>)
