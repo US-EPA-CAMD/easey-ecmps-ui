6d3545facb54b52ec5a3749b88f0cd5502488c61
Corey W <106772898+cw-cvp@users.noreply.github.com>
2022-10-10 12:25:19 -0400

    remove console log

--- src/utils/selectors/QACert/assert.js
+++ src/utils/selectors/QACert/assert.js
@@ -38,13 +38,6 @@ export const getDataTableApis = async (name, location, id, extraIdsArr) => {
       });
 
     case rataRunData:
-      console.log(
-        "name, location, id, extraIdsArr",
-        name,
-        location,
-        id,
-        extraIdsArr
-      );
       return qaApi
         .getRataRunData(extraIdsArr[0], extraIdsArr[1], extraIdsArr[2], id)
         .catch((error) => {
