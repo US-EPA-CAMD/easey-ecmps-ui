383395974987268e31afcfc732878b55e025770a
sdhawan16 <sejaldhawan@cvpcorp.com>
2022-10-12 15:28:40 -0400

    removed daily, hourly, and mats sections from emissions nav panel

--- src/utils/constants/moduleTitles.js
+++ src/utils/constants/moduleTitles.js
@@ -2,6 +2,4 @@ export const monitoring_plans_module = "Monitoring Plans";
 export const qa_Certifications_Test_Summary_Module =
   "QA Certifications Test Data ";
 export const export_Module = "Export Data";
-export const emissions_daily_module = "Emissions Daily";
-export const emissions_hourly_module = "Emissions Hourly";
-export const emissions_mats_module = "Emissions MATS";
+export const emissions_module = "Emissions";
