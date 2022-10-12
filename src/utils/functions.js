e86f87171c55966db77808f9aa062211ebed1e46
Ethan Glover <hello@ethang.email>
2022-10-12 13:45:17 -0500

    :sparkles: feature(4014): Export Section Functionality in Emissions Module
    * Added emissions export / download.
    * Added emissions download to export button.
    * Export button will be disabled while downloading.
    * EsLint/Prettier Changes

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,44 +1,38 @@
 export const parseBool = (value, defaultValue = false) => {
-
-  if (typeof value == 'number' || value instanceof Number) {
-    return (value > 0);
+  if (typeof value == "number" || value instanceof Number) {
+    return value > 0;
   }
 
-  if (typeof value == 'boolean' || value instanceof Boolean) {
+  if (typeof value == "boolean" || value instanceof Boolean) {
     return value;
   }
 
-  if (typeof value == 'string' || value instanceof String) {
+  if (typeof value == "string" || value instanceof String) {
     value = value.trim().toLowerCase();
-    if (value === 'true' || value === 'false') {
-      return (value === 'true');
+    if (value === "true" || value === "false") {
+      return value === "true";
     }
   }
-  
-  return defaultValue;
-}
 
-export const getConfigValue = (key, defaultValue = '') => {
+  return defaultValue;
+};
+
+export const getConfigValue = (key, defaultValue = "") => {
   let returnValue;
 
   if (window._env_ && window._env_[key]) {
     returnValue = window._env_[key];
-  }
-  else if (!returnValue && process.env[key]) {
+  } else if (!returnValue && process.env[key]) {
     returnValue = process.env[key];
   }
 
   return returnValue || defaultValue;
 };
 
-export const getConfigValueNumber = (key, defaultValue = '') => {
-  return Number(
-    getConfigValue(key, defaultValue)
-  );
-}
+export const getConfigValueNumber = (key, defaultValue = "") => {
+  return Number(getConfigValue(key, defaultValue));
+};
 
-export const getConfigValueBoolean = (key, defaultValue = '') => {
-  return parseBool(
-    getConfigValue(key, defaultValue)
-  );
-}
+export const getConfigValueBoolean = (key, defaultValue = "") => {
+  return parseBool(getConfigValue(key, defaultValue));
+};
