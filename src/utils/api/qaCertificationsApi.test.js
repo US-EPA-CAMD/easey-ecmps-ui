152f68687ed115f6ccba73d02995b48a42ff837f
RobertB-CVP <robertboehme@cvpcorp.com>
2022-10-18 12:22:25 -0400

    added unit tests

--- src/utils/api/qaCertificationsApi.test.js
+++ src/utils/api/qaCertificationsApi.test.js
@@ -111,3 +111,16 @@ describe('RATA traverse CRUD operations', () => {
 
   })
 })
+
+describe('Fuel Flow To Load CRUD operations', () => {
+  test('deleteFuelFlowToLoadData', async () => {
+    const path = `/locations/${locId}/test-summary/${testSumId}/fuel-flow-to-load-tests/${id}`;
+    const deleteRataTraverseUrl = getApiUrl(path);
+    mock.onDelete(deleteRataTraverseUrl).reply(200, 'deleted')
+
+    const resp = await qaCert.deleteRataTraverseData(locId, testSumId, id)
+
+    expect(mock.history.delete.length).toBe(1)
+    expect(resp.data).toStrictEqual('deleted')
+  })
+})
