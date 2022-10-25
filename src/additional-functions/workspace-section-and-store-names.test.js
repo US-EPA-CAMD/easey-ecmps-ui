e2bd931ffb2727b5777382e23cd89a3b1f1e6ba5
Quang Bui <quangbui@cvpcorp.com>
2022-10-25 10:49:24 -0400

    unit test and removed files

---
+++ src/additional-functions/workspace-section-and-store-names.test.js
@@ -0,0 +1,20 @@
+import { convertSectionToStoreName } from "./workspace-section-and-store-names";
+
+describe("workspace store names", () => {
+  const MONITORING_PLAN_STORE_NAME = "monitoringPlans";
+  const QA_CERT_TEST_SUMMARY_STORE_NAME = "qaCertTestSummary";
+  const EXPORT_STORE_NAME = "export";
+  const EMISSIONS_STORE_NAME = "emissions";
+
+  it("should test with valid input", () => {
+    expect(convertSectionToStoreName(MONITORING_PLAN_STORE_NAME)).toBe(
+      "monitoringPlans"
+    );
+    expect(convertSectionToStoreName(QA_CERT_TEST_SUMMARY_STORE_NAME)).toBe(
+      "qaCertTestSummary"
+    );
+    expect(convertSectionToStoreName(EXPORT_STORE_NAME)).toBe("export");
+    expect(convertSectionToStoreName(EMISSIONS_STORE_NAME)).toBe("emissions");
+    expect(convertSectionToStoreName('false')).toBe("");
+  });
+});
