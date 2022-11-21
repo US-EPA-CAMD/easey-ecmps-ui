64ffaafd0e78b5fa2cde7b313d804c8af6ac4a44
Bill Miller <snookblast@gmail.com>
2022-11-21 13:32:54 -0500

    Fix Failing Unit Tests

--- src/components/ReviewAndSubmit/ReviewAndSubmit.test.js
+++ src/components/ReviewAndSubmit/ReviewAndSubmit.test.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
+import { render } from "@testing-library/react";
 
-import ReviewAndSubmit from './ReviewAndSubmit';
+import ReviewAndSubmit from "./ReviewAndSubmit";
 
-describe('Data preview component', () => {
+describe("Data preview component", () => {
   let query;
   beforeEach(() => {
     query = render(<ReviewAndSubmit />);
   });
 
-  it('renders review and submit component properly', () => {
-    const { getByRole } = query;
-    expect(getByRole('checkbox', {name: 'Monitoring Plan'})).toBeInTheDocument();
-    expect(getByRole('checkbox', {name: 'QA & Certification'})).toBeInTheDocument();
-    expect(getByRole('checkbox', {name: 'Emissions'})).toBeInTheDocument();
-    expect(getByRole('radio', {name:'Exclude Files with Critical Errors'})).toBeInTheDocument();
-    expect(getByRole('radio', {name:'Include Files with Critical Errors'})).toBeInTheDocument();
-    expect(getByRole('textbox', {name: 'Facility ID(s)'})).toBeInTheDocument();
-    expect(getByRole('textbox', {name: 'Configuration(s)'})).toBeInTheDocument();
-    expect(getByRole('textbox', {name:'Reporting Period(s)'})).toBeInTheDocument();
+  it("renders review and submit component properly", () => {
+    const { getByRole, getByText } = query;
+    expect(
+      getByRole("checkbox", { name: "Monitoring Plan" })
+    ).toBeInTheDocument();
+    expect(
+      getByRole("checkbox", { name: "QA & Certification" })
+    ).toBeInTheDocument();
+    expect(getByRole("checkbox", { name: "Emissions" })).toBeInTheDocument();
+    expect(
+      getByRole("radio", { name: "Exclude Files with Critical Errors" })
+    ).toBeInTheDocument();
+    expect(
+      getByRole("radio", { name: "Include Files with Critical Errors" })
+    ).toBeInTheDocument();
+    expect(getByText("Facility ID(s)")).toBeInTheDocument();
+    expect(getByText("Configuration(s)")).toBeInTheDocument();
+    expect(getByText("Reporting Period(s)")).toBeInTheDocument();
   });
 });
