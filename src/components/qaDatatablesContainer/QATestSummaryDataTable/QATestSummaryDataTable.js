2a651c39465d265f97d91a6b8c7e9d9e08bfa1f3
Quang Bui <quangbui@cvpcorp.com>
2022-12-05 12:05:13 -0500

    auto selected unit id on create

--- src/components/qaDatatablesContainer/QATestSummaryDataTable/QATestSummaryDataTable.js
+++ src/components/qaDatatablesContainer/QATestSummaryDataTable/QATestSummaryDataTable.js
@@ -19,7 +19,7 @@ import {
   qaFuelFlowToLoadBaselineProps,
   qaFlowToLoadCheckProps,
   qaOnOffCalibrationProps,
-  qaCalibrationInjectionProps
+  qaCalibrationInjectionProps,
 } from "../../../additional-functions/qa-dataTable-props";
 import {
   attachChangeEventListeners,
@@ -40,7 +40,10 @@ import * as dmApi from "../../../utils/api/dataManagementApi";
 import { organizePrefilterMDMData } from "../../../additional-functions/retrieve-dropdown-api";
 
 import QAExpandableRowsRender from "../QAExpandableRowsRender/QAExpandableRowsRender";
-import { returnsFocusDatatableViewBTN, returnsFocusToAddBtn } from "../../../additional-functions/ensure-508.js";
+import {
+  returnsFocusDatatableViewBTN,
+  returnsFocusToAddBtn,
+} from "../../../additional-functions/ensure-508.js";
 
 // contains test summary data table
 
@@ -302,7 +305,7 @@ const QATestSummaryDataTable = ({
   };
   // Executed when "View" action is clicked
   const openModal = (row, bool, create, index) => {
-    let selectedData = null;
+    let selectedData = {};
     setCreateNewData(create);
     if (dataPulled.length > 0 && !create) {
       selectedData = dataPulled.filter(
@@ -315,17 +318,19 @@ const QATestSummaryDataTable = ({
         controlInputs.unitId = [
           "Unit or Stack Pipe ID",
           "nonFilteredDropdown",
-          "",
+          selectedLocation.name,
           "",
         ];
       } else {
         controlInputs.stackPipeId = [
           "Unit or Stack Pipe ID",
           "nonFilteredDropdown",
-          "",
+          selectedLocation.name,
           "",
         ];
       }
+
+      selectedData.locationName = selectedLocation.name;
     }
     let mainDropdownName = "";
     let hasMainDropdown = false;
@@ -348,7 +353,10 @@ const QATestSummaryDataTable = ({
           (element, index, arr) => o.code === element[mainDropdownName]
         )
       );
-      if (mainDropdownResult.length > 1 && !mainDropdownResult.includes({ code: "", name: selectText })) {
+      if (
+        mainDropdownResult.length > 1 &&
+        !mainDropdownResult.includes({ code: "", name: selectText })
+      ) {
         mainDropdownResult.unshift({ code: "", name: selectText });
       }
     } else {
@@ -377,7 +385,7 @@ const QATestSummaryDataTable = ({
     );
     setSelectedModalData(modalData);
 
-    setClickedIndex(index)
+    setClickedIndex(index);
 
     setShow(true);
     setTimeout(() => {
@@ -394,7 +402,7 @@ const QATestSummaryDataTable = ({
       executeOnClose();
     }
     if (createNewData) {
-      returnsFocusToAddBtn(dataTableName.replaceAll(" ", "-"))
+      returnsFocusToAddBtn(dataTableName.replaceAll(" ", "-"));
     }
   };
 
@@ -402,15 +410,18 @@ const QATestSummaryDataTable = ({
     setShow(false);
     removeChangeEventListeners(".modalUserInput");
 
-    const updatedData = getTestSummary(data ? data : [], columns)
-    const idx = updatedData.findIndex(d => d.id === createdId)
+    const updatedData = getTestSummary(data ? data : [], columns);
+    const idx = updatedData.findIndex((d) => d.id === createdId);
 
     if (idx >= 0) {
-      returnsFocusDatatableViewBTN(dataTableName.replaceAll(" ", "-"), idx)
-      setCreatedId(null)
-      setCreateNewData(false)
+      returnsFocusDatatableViewBTN(dataTableName.replaceAll(" ", "-"), idx);
+      setCreatedId(null);
+      setCreateNewData(false);
     } else {
-      returnsFocusDatatableViewBTN(dataTableName.replaceAll(" ", "-"), clickedIndex)
+      returnsFocusDatatableViewBTN(
+        dataTableName.replaceAll(" ", "-"),
+        clickedIndex
+      );
     }
   };
 
@@ -418,7 +429,7 @@ const QATestSummaryDataTable = ({
     const { id, locationId } = row;
     const resp = await deleteQATestSummary(locationId, id);
     if (resp.status === 200) {
-      returnsFocusToAddBtn(dataTableName.replaceAll(" ", "-"))
+      returnsFocusToAddBtn(dataTableName.replaceAll(" ", "-"));
       const dataPostRemove = qaTestSummary.filter(
         (rowData) => rowData.id !== id
       );
@@ -508,7 +519,7 @@ const QATestSummaryDataTable = ({
       })
       .catch((error) => {
         console.error("error", error);
-        returnsFocusToAddBtn(dataTableName.replaceAll(" ", "-"))
+        returnsFocusToAddBtn(dataTableName.replaceAll(" ", "-"));
       });
   };
 
@@ -662,7 +673,9 @@ const QATestSummaryDataTable = ({
           />
         );
       default:
-        console.log(`case testTypeGroupCode of ${testTypeGroupCode} not implemented`)
+        console.log(
+          `case testTypeGroupCode of ${testTypeGroupCode} not implemented`
+        );
         return null;
     }
   };
