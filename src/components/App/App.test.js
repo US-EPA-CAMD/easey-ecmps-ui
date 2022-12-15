0d42d48c2a65ac64e0a52f6e2eee56e232838dc7
Quang Bui <quangbui@cvpcorp.com>
2022-12-14 23:25:30 -0500

    updated app.js

---
+++ src/components/App/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {ReactMarkdown} from "react-markdown";
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
