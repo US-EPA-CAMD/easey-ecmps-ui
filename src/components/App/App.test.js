44acd271eb11e5f78d43689156ccedbe2107abf1
AbuFofanah <abufofanah@cvpcorp.com>
2022-12-21 17:20:49 -0500

    updated app test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import {ReactMarkdown} from "react-markdown";
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+jest.mock('react-markdown', () => ({ children }) => <>{children}</>);
+jest.mock('remark-gfm', () => () => {});
+// jest.useFakeTimers();
+// jest.spyOn(global, 'setInterval');
+// jest.spyOn(global, 'setTimeout');
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+  connect: (mapStateToProps, mapDispatchToProps) => (ReactComponent) => ({
+    mapStateToProps,
+    mapDispatchToProps,
+    ReactComponent,
+  }),
+}));
+const mockApiCall = jest.fn().mockResolvedValue({
+  data: [],
+});
+jest.mock('../../utils/api/monitoringPlansApi', () => ({
+  getCheckedOutLocations: () => mockApiCall(),
+}));
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  expect(1).toBe(1);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  screen.debug();
 });
