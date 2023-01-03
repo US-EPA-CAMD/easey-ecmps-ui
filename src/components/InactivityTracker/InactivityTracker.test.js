6ee8a575a84daf855b864c714288d7cd1a3e9232
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-01-03 16:47:55 -0500

    remove commented out code

--- src/components/InactivityTracker/InactivityTracker.test.js
+++ src/components/InactivityTracker/InactivityTracker.test.js
@@ -1,123 +1,18 @@
-// import React from "react";
-// import { config } from "../../config";
-// import { render, unmountComponentAtNode } from "react-dom";
-// import { act } from "react-dom/test-utils";
-// import { InactivityTracker } from "./InactivityTracker";
-// import { logOut } from "../../utils/api/easeyAuthApi";
-// import { checkoutAPI } from "../../additional-functions/checkout";
-
-// jest.mock("../../utils/api/easeyAuthApi");
-// logOut.mockImplementation(() => Promise.resolve(""));
-
-// jest.mock("../../additional-functions/checkout");
-// checkoutAPI.mockImplementation(() => Promise.resolve(""));
-
-// let container = null;
-// describe("Inactivity Tracker", () => {
-//   beforeEach(() => {
-//     // setup a DOM element as a render target
-//     container = document.createElement("div");
-//     document.body.appendChild(container);
-//   });
-
-//   afterEach(() => {
-//     // cleanup on exiting
-//     unmountComponentAtNode(container);
-//     container.remove();
-//     container = null;
-//   });
-
-//   test("Doesn't sign out before inactivity expired", () => {
-//     jest.useFakeTimers();
-
-//     act(() => {
-//       render(
-//         <InactivityTracker openedFacilityTabs={{}} setCheckout={jest.fn()} />,
-//         container
-//       );
-//     });
-
-//     expect(logOut).not.toHaveBeenCalled();
-//   });
-
-//   test("Don't sign out if check out and time hasn't expired", () => {
-//     const currentCheckedOut = { checkout: true, selectedConfig: { id: 0 } };
-
-//     jest.useFakeTimers();
-
-//     act(() => {
-//       render(
-//         <InactivityTracker
-//           openedFacilityTabs={{ currentCheckedOut }}
-//           setCheckout={jest.fn()}
-//         />,
-//         container
-//       );
-//     });
-
-//     expect(logOut).not.toHaveBeenCalled();
-//   });
-
-//   test("Sign out after inactivity expired", () => {
-//     jest.useFakeTimers();
-
-//     act(() => {
-//       render(
-//         <InactivityTracker openedFacilityTabs={{}} setCheckout={jest.fn()} />,
-//         container
-//       );
-//     });
-
-//     act(() => {
-//       jest.advanceTimersByTime(config.app.inactivityLogoutDuration + 1001);
-//     });
-
-//     expect(logOut).toHaveBeenCalled();
-//   });
-
-//   test("Sign out after expired and record checked out", () => {
-//     jest.useFakeTimers();
-
-//     const currentCheckedOut = { checkout: true, selectedConfig: { id: 0 } };
-
-//     act(() => {
-//       render(
-//         <InactivityTracker
-//           openedFacilityTabs={{ currentCheckedOut }}
-//           setCheckout={jest.fn()}
-//         />,
-//         container
-//       );
-//     });
-
-//     act(() => {
-//       jest.advanceTimersByTime(config.app.inactivityDuration + 1001);
-//     });
-
-//     expect(logOut).toHaveBeenCalled();
-//   });
-// });
-// import React from "react";
-// import { config } from "../../config";
-// import { render, unmountComponentAtNode } from "react-dom";
-// import { act } from "react-dom/test-utils";
-// import { InactivityTracker } from "./InactivityTracker";
-// import { logOut } from "../../utils/api/easeyAuthApi";
-// import { checkoutAPI } from "../../additional-functions/checkout";
 import React from "react";
-import { render, fireEvent, screen, act, waitForElement } from "@testing-library/react";
+import { render, fireEvent, screen, act } from "@testing-library/react";
+import { Provider } from "react-redux";
+
 import {
   InactivityTracker,
   mapStateToProps,
   mapDispatchToProps,
 } from "./InactivityTracker";
-import { Provider } from "react-redux";
 import configureStore from "../../store/configureStore.dev";
 import config from "../../config";
 import { logOut } from "../../utils/api/easeyAuthApi";
 import userEvent from "@testing-library/user-event";
+
 const store = configureStore();
-const axios = require("axios");
 jest.mock("axios");
 
 jest.mock("../../utils/api/easeyAuthApi");
@@ -201,25 +96,6 @@ describe("InactivityTracker", () => {
     expect(logOut).not.toHaveBeenCalled()
     expect(inactivityMsg).not.toBeInTheDocument()
   });
-
-  test("when logout modal timer counts down then user is logged out", () => {
-    config.countdownDuration = 0
-    render(
-      <Provider store={store}>
-        <InactivityTracker {...noCheckoutProps} />{" "}
-      </Provider>
-    )
-
-    act(() => {
-      jest.advanceTimersByTime(config.app.inactivityLogoutDuration);
-    });
-
-    act(() => {
-      jest.advanceTimersByTime(60000)
-    })
-
-    expect(logOut).toHaveBeenCalled()
-  })
 
   test("mapDispatchToProps calls the appropriate action", async () => {
     // mock the 'dispatch' object
