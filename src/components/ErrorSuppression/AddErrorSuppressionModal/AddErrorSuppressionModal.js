d767de6ca590bf302c8d70efcee702c762fc9cce
Fahim Anwari <fahimanwari@cvpcorp.com>
2023-01-19 00:19:59 -0500

    * Started adding Trussworks modal to AddErrorSuppressionModal component

--- src/components/ErrorSuppression/AddErrorSuppressionModal/AddErrorSuppressionModal.js
+++ src/components/ErrorSuppression/AddErrorSuppressionModal/AddErrorSuppressionModal.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, {useRef} from "react";
+import { Modal, ModalFooter, Button, ButtonGroup } from "@trussworks/react-uswds";
 
 export const AddErrorSupressionModal = () => {
+
     return (
-        <div>AddErrorSupression Component</div>
+        <div>
+            <Modal
+                id="example-modal-1"
+                aria-labelledby="modal-1-heading"
+                aria-describedby="modal-1-description">
+
+                <ModalHeading id="modal-1-heading">
+                    Are you sure you want to continue?
+                </ModalHeading>
+
+                <div className="usa-prose">
+                    <p id="modal-1-description">
+                        You have unsaved changes that will be lost.
+                    </p>
+                </div>
+
+                <ModalFooter>
+                    <ButtonGroup>
+                        <Button closer>
+                            Save and Close
+                        </Button>
+                        <Button
+                            className="padding-105 text-center">
+                            Go back
+                        </Button>
+                    </ButtonGroup>
+                </ModalFooter>
+            </Modal>
+        </div>
     )
 }
