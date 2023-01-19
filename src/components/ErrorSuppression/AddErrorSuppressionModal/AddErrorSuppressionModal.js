e13d1bf41c1b3135e1d94e2522dd6e293879203f
Fahim Anwari <fahimanwari@cvpcorp.com>
2023-01-19 15:32:04 -0500

    Finished adding Modal
    Added dark background
    
    Added functionality for opening and closing modal

--- src/components/ErrorSuppression/AddErrorSuppressionModal/AddErrorSuppressionModal.js
+++ src/components/ErrorSuppression/AddErrorSuppressionModal/AddErrorSuppressionModal.js
@@ -1,36 +1,19 @@
-import React, {useRef} from "react";
-import { Modal, ModalFooter, Button, ButtonGroup } from "@trussworks/react-uswds";
+import React, { useRef } from "react";
+// import { Modal, ModalFooter, ModalHeading, Button, ButtonGroup, ModalToggleButton, ModalRef } from "@trussworks/react-uswds";
+import Modal from "../../Modal/Modal";
 
-export const AddErrorSupressionModal = () => {
+export const AddErrorSupressionModal = ({ showModal, close }) => {
+
+    const saveFunc = () => {
+
+        close();
+    }
 
     return (
         <div>
-            <Modal
-                id="example-modal-1"
-                aria-labelledby="modal-1-heading"
-                aria-describedby="modal-1-description">
+            <div className="usa-overlay is-visible"></div>
+            <Modal show={showModal} save={saveFunc} exitBTN={"Save and Close"} showSave title={"Add Error Suppression"} close={close}>
 
-                <ModalHeading id="modal-1-heading">
-                    Are you sure you want to continue?
-                </ModalHeading>
-
-                <div className="usa-prose">
-                    <p id="modal-1-description">
-                        You have unsaved changes that will be lost.
-                    </p>
-                </div>
-
-                <ModalFooter>
-                    <ButtonGroup>
-                        <Button closer>
-                            Save and Close
-                        </Button>
-                        <Button
-                            className="padding-105 text-center">
-                            Go back
-                        </Button>
-                    </ButtonGroup>
-                </ModalFooter>
             </Modal>
         </div>
     )
