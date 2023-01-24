2748d9f328015a50400a4e4d66b2664cf9bdf2b2
Fahim Anwari <fahimanwari@cvpcorp.com>
2023-01-24 02:20:49 -0500

    Added rest of the Time Criteria fields
    getReportingPeriods() in HeaderInfo is now exported as it is being reused

--- src/components/ErrorSuppression/AddErrorSuppressionModal/AddErrorSuppressionModal.js
+++ src/components/ErrorSuppression/AddErrorSuppressionModal/AddErrorSuppressionModal.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { GridContainer, Grid, Dropdown, Label } from "@trussworks/react-uswds";
+import React, { useState, useEffect, useMemo } from "react";
+import { GridContainer, Grid, Dropdown, Label, DatePicker, TextInput, Checkbox } from "@trussworks/react-uswds";
 // import { Modal, ModalFooter, ModalHeading, Button, ButtonGroup, ModalToggleButton, ModalRef } from "@trussworks/react-uswds";
 import Modal from "../../Modal/Modal";
 import MultiSelectCombobox from "../../MultiSelectCombobox/MultiSelectCombobox";
+import { getReportingPeriods } from "../../HeaderInfo/HeaderInfo";
 
 export const AddErrorSupressionModal = ({ showModal, close }) => {
 
@@ -23,10 +24,17 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
     const [selectedBeginHour, setSelectedBeginHour] = useState();
     const [selectedEndDate, setSelectedEndDate] = useState();
     const [selectedEndHour, setSelectedEndHour] = useState();
-
+    const [selectedNotes, setSelectedNotes] = useState();
+    const [selectedIsHistorical, setSelectedIsHistorical] = useState();
+    const [selectedBeginQuarter, setSelectedBeginQuarter] = useState();
+    const [selectedEndQuarter, setSelectedEndQuarter] = useState();
 
 
     const [locationData, setLocationData] = useState([]);
+
+    const hoursInADay = [...Array(24).keys()]
+
+    const yearQuarters = useMemo(getReportingPeriods, [])
 
     useEffect(() => {
         setLocationData([
@@ -38,6 +46,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
             }
         ])
     }, []);
+
 
     const saveFunc = () => {
 
@@ -63,13 +72,13 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
     return (
         <div>
             <div className="usa-overlay is-visible"></div>
-            <Modal show={showModal} save={saveFunc} exitBTN={"Save and Close"} showSave title={"Add Error Suppression"} close={close}>
+            <Modal show={showModal} save={saveFunc} exitBTN={"Save and Close"} showSave title={"Add Error Suppression"} close={close} width={"1024px"}>
                 <GridContainer className='margin-left-1'>
                     <Grid row gap={2}>
                         <h3>Check Result</h3>
                     </Grid>
                     <Grid row gap={2}>
-                        <Grid col={4}>
+                        <Grid col={5}>
                             <Label test-id={"add-check-type-label"} htmlFor={"add-check-type"}>
                                 Check Type
                             </Label>
@@ -83,7 +92,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                                 <option>Coming Soon...</option>
                             </Dropdown>
                         </Grid>
-                        <Grid col={2}>
+                        <Grid col={5}>
                             <Label test-id={"add-check-number-label"} htmlFor={"add-check-number"}>
                                 Check Number
                             </Label>
@@ -99,7 +108,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                         </Grid>
                     </Grid>
                     <Grid row gap={2} className="margin-top-2">
-                        <Grid col={4}>
+                        <Grid col={5}>
                             <Label test-id={"add-check-result-label"} htmlFor={"add-check-result"}>
                                 Check Result
                             </Label>
@@ -118,7 +127,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                         <h3>Severity and Reason</h3>
                     </Grid>
                     <Grid row gap={2}>
-                        <Grid col={4}>
+                        <Grid col={5}>
                             <Label test-id={"add-type"} htmlFor={"add-type"}>
                                 Type
                             </Label>
@@ -132,7 +141,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                                 <option>Coming Soon...</option>
                             </Dropdown>
                         </Grid>
-                        <Grid col={4}>
+                        <Grid col={5}>
                             <Label test-id={"add-reason"} htmlFor={"add-reason"}>
                                 Reason
                             </Label>
@@ -151,7 +160,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                         <h3>Targeting</h3>
                     </Grid>
                     <Grid row gap={2}>
-                        <Grid col={4}>
+                        <Grid col={5}>
                             <Label test-id={"add-facility-name-label"} htmlFor={"add-facility-name"}>
                                 Facility Name
                             </Label>
@@ -165,7 +174,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                                 <option>Coming Soon...</option>
                             </Dropdown>
                         </Grid>
-                        <Grid col={4}>
+                        <Grid col={5}>
                             <MultiSelectCombobox
                                 items={locationData}
                                 label="Location Name"
@@ -177,7 +186,7 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                         </Grid>
                     </Grid>
                     <Grid row gap={2}>
-                        <Grid col={4}>
+                        <Grid col={5}>
                             <Label test-id={"add-fuel-type"} htmlFor={"add-fuel-type"}>
                                 Fuel Type
                             </Label>
@@ -198,26 +207,143 @@ export const AddErrorSupressionModal = ({ showModal, close }) => {
                     {/* These time criteria rows will be swapable later on */}
                     <Grid row gap={2}>
                         <Grid col={3} >
-                            <div className="margin-left-4">
-                                <Label
-                                    htmlFor="add-begin-date"
-                                    id="add-begin-date"
-                                >
-                                    Begin Date
-                                </Label>
-                                <DatePicker
-                                    aria-labelledby="add-begin-date"
-                                    id="add-begin-date"
-                                    name="add-begin-date"
-                                    value={selectedBeginDate}
-                                />
-                            </div>
+                            <Label
+                                htmlFor="add-begin-date"
+                                id="add-begin-date"
+                            >
+                                Begin Date
+                            </Label>
+                            <DatePicker
+                                aria-labelledby="add-begin-date"
+                                id="add-begin-date"
+                                name="add-begin-date"
+                                value={selectedBeginDate}
+                            />
+                        </Grid>
+                        <Grid col={2}>
+                            <Label test-id={"add-begin-hour"} htmlFor={"add-begin-hour"}>
+                                Begin Hour
+                            </Label>
+                            <Dropdown
+                                id={"add-begin-hour"}
+                                name={"add-begin-hour"}
+                                epa-testid={"add-begin-hour"}
+                                data-testid={"add-begin-hour"}
+                                value={selectedBeginHour}
+                                className="width-10"
+                            >
+                                {hoursInADay.map((h) =>
+                                    <option>{h}</option>
+                                )}
+                            </Dropdown>
+
+                        </Grid>
+                        <Grid col={3} >
+                            <Label
+                                htmlFor="add-end-date"
+                                id="add-end-date"
+                            >
+                                End Date
+                            </Label>
+                            <DatePicker
+                                aria-labelledby="add-end-date"
+                                id="add-end-date"
+                                name="add-end-date"
+                                value={selectedEndDate}
+                            />
+                        </Grid>
+                        <Grid col={3}>
+                            <Label test-id={"add-end-hour"} htmlFor={"add-end-hour"}>
+                                End Hour
+                            </Label>
+                            <Dropdown
+                                id={"add-end-hour"}
+                                name={"add-end-hour"}
+                                epa-testid={"add-end-hour"}
+                                data-testid={"add-end-hour"}
+                                value={selectedEndHour}
+                                className="width-10"
+                            >
+                                {hoursInADay.map((h) =>
+                                    <option>{h}</option>
+                                )}
+                            </Dropdown>
                         </Grid>
                     </Grid>
                     <Grid row gap={2}>
+                        <Grid col={3} >
+                            <Label
+                                htmlFor="add-begin-date-2"
+                                id="add-begin-date-2"
+                            >
+                                Begin Date
+                            </Label>
+                            <DatePicker
+                                aria-labelledby="add-begin-date-2"
+                                id="add-begin-date-2"
+                                name="add-begin-date-2"
+                                value={selectedBeginDate}
+                            />
+                        </Grid>
+                        <Grid col={3} >
+                            <Label
+                                htmlFor="add-end-date-2"
+                                id="add-end-date-2"
+                            >
+                                End Date
+                            </Label>
+                            <DatePicker
+                                aria-labelledby="add-end-date-2"
+                                id="add-end-date-2"
+                                name="add-end-date-2"
+                                value={selectedEndDate}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid row gap={2}>
+                        <Grid col={3}>
+                            <Checkbox id="add-is-historical" name="add-is-historical" label="Historical" value={selectedIsHistorical} />
+                        </Grid>
+                    </Grid>
+                    <Grid row gap={4}>
+                        <Grid col={3}>
+                            <Label test-id={"add-begin-quarter"} htmlFor={"add-begin-quarter"}>
+                                Begin Quarter
+                            </Label>
+                            <Dropdown
+                                id={"add-begin-quarter"}
+                                name={"add-begin-quarter"}
+                                epa-testid={"add-begin-quarter"}
+                                data-testid={"add-begin-quarter"}
+                                value={selectedBeginQuarter}
+                            >
+                                {yearQuarters.map((yearquarter) =>
+                                    <option>{yearquarter}</option>
+                                )}
+                            </Dropdown>
+                        </Grid>
+                        <Grid col={3}>
+                            <Label test-id={"add-end-quarter"} htmlFor={"add-end-quarter"}>
+                                End Quarter
+                            </Label>
+                            <Dropdown
+                                id={"add-end-quarter"}
+                                name={"add-end-quarter"}
+                                epa-testid={"add-end-quarter"}
+                                data-testid={"add-end-quarter"}
+                                value={selectedEndQuarter}
+                            >
+                                {yearQuarters.map((yearquarter) =>
+                                    <option>{yearquarter}</option>
+                                )}
+                            </Dropdown>
+                        </Grid>
+                    </Grid>
+
+                    <Grid row gap={2}>
                         <Grid col={10}>
                             <Label htmlFor="add-notes" id="add-notes-label">Notes</Label>
-                            <TextInput id="add-notes" name="add-notes" type="text" />
+                            <TextInput className="maxw-full" id="add-notes" name="add-notes" type="text" value={selectedNotes} />
                         </Grid>
                     </Grid>
                 </GridContainer>
