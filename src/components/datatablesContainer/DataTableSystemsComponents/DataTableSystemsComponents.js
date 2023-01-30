444d38f2ab6ebe7dbd77405fe649634dea1fa9c7
AbuFofanah <abufofanah@cvpcorp.com>
2023-01-30 17:04:20 -0500

    build error fix

--- src/components/datatablesContainer/DataTableSystemsComponents/DataTableSystemsComponents.js
+++ src/components/datatablesContainer/DataTableSystemsComponents/DataTableSystemsComponents.js
@@ -229,10 +229,10 @@ export const DataTableSystemsComponents = ({
         for (const modalDetailData of selectedComponentsModalData) {
           if (modalDetailData[4] === "dropdown") {
             const selectedCodes = result[0];
-            const filteredOutSubDropdownOptions = systemComponentsMdmData[
+            let filteredOutSubDropdownOptions = systemComponentsMdmData[
               modalDetailData[0]
             ] || [];
-            filteredOutSubDropdownOptions.filter((option) =>
+            filteredOutSubDropdownOptions = filteredOutSubDropdownOptions.filter((option) =>
               selectedCodes[modalDetailData[0]].includes(option.code)
             );
 
