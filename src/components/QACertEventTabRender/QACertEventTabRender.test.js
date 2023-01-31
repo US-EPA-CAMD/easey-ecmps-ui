0ecdc425425a2050e03c6b0ee659d81390da4dbc
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2023-01-31 15:23:08 -0500

    Created test file for QACertEventTabRender.js

---
+++ src/components/QACertEventTabRender/QACertEventTabRender.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import QACertEventTabRender from './QACertEventTabRender';
+
+jest.mock('../QACertEventHeaderInfo/QACertEventHeaderInfo', () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return <div />;
+    },
+  };
+});
+jest.mock(
+  '../qaDatatablesContainer/QACertEventTestExmpDataTable/QACertEventTestExmpDataTable',
+  () => {
+    return {
+      __esModule: true,
+      default: () => {
+        return <div />;
+      },
+    };
+  }
+);
+
+const selectedConfig = {
+  id: 'MDC-7C15B3D1B20542C3B54DD57F03A516E5',
+  name: '110',
+  locations: [
+    {
+      id: '655',
+      name: '110',
+      type: 'Unit',
+      active: false,
+      retireDate: null,
+    },
+  ],
+};
+
+const props = {
+  title: ' ( test ) ',
+  user: { firstName: 'test' },
+  locations: selectedConfig.locations,
+  selectedConfig: selectedConfig,
+  setSectionSelect: jest.fn(),
+  setLocationSelect: jest.fn(),
+  sectionSelect: [3, 'Methods'],
+  locationSelect: [0, '65'],
+  orisCode: '5',
+  checkoutState: true,
+  configID: selectedConfig.id,
+};
+test('tests QACertEventTabRender', () => {
+  const { container } = render(<QACertEventTabRender {...props} />);
+  expect(container).not.toBeUndefined();
+});
