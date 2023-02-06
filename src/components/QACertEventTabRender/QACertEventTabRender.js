fe8ae4658701ff1ff528629620230f79c4cd1a01
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2023-02-06 15:12:40 -0500

    Adding unit tests

--- src/components/QACertEventTabRender/QACertEventTabRender.js
+++ src/components/QACertEventTabRender/QACertEventTabRender.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import '../MonitoringPlanTab/MonitoringPlanTab.scss';
-import QACertEventHeaderInfo from '../QACertEventHeaderInfo/QACertEventHeaderInfo';
+import React, { useEffect, useState } from "react";
+import QACertEventHeaderInfo from "../QACertEventHeaderInfo/QACertEventHeaderInfo";
 import QACertEventTestExmpDataTable from "../qaDatatablesContainer/QACertEventTestExmpDataTable/QACertEventTestExmpDataTable";
 
 export const QACertEventTabRender = ({
@@ -22,9 +21,9 @@ export const QACertEventTabRender = ({
   checkoutState,
 }) => {
   const [updateRelatedTables, setUpdateRelatedTables] = useState(false);
-  useEffect(()=>{
+  useEffect(() => {
     setSectionSelect([0, "QA Certification Event"]);
-  },[])
+  }, []);
   return (
     <div className=" padding-top-0">
       <div className="grid-row">
@@ -48,18 +47,18 @@ export const QACertEventTabRender = ({
       </div>
       <hr />
       <QACertEventTestExmpDataTable
-          locationSelectValue={locationSelect ? locationSelect[1] : 0}
-          user={user}
-          sectionSelect={sectionSelect}
-          selectedLocation={{
-            name: locations[locationSelect[0]]["name"],
-            stackPipeId: locations[locationSelect[0]]["stackPipeId"],
-            unitId: locations[locationSelect[0]]["unitId"],
-          }}
-          locations={locations}
-          selectedTestCode={selectedTestCode}
-          isCheckedOut={checkoutState}
-        />
+        locationSelectValue={locationSelect ? locationSelect[1] : 0}
+        user={user}
+        sectionSelect={sectionSelect}
+        selectedLocation={{
+          name: locations[locationSelect[0]]["name"],
+          stackPipeId: locations[locationSelect[0]]["stackPipeId"],
+          unitId: locations[locationSelect[0]]["unitId"],
+        }}
+        locations={locations}
+        selectedTestCode={selectedTestCode}
+        isCheckedOut={checkoutState}
+      />
     </div>
   );
 };
