9b3d273248ba7a64907e1896325fdcba148469fb
sdhawan16 <sejaldhawan@cvpcorp.com>
2023-02-07 14:54:27 -0500

    modified formatting

--- src/components/ErrorSuppression/ErrorSuppressionFilters/ErrorSuppressionFilters.js
+++ src/components/ErrorSuppression/ErrorSuppressionFilters/ErrorSuppressionFilters.js
@@ -189,190 +189,190 @@ export const ErrorSuppressionFilters = () => {
     }
 
     return (
-      <GridContainer className='padding-left-0 margin-left-0 padding-right-0'>
-          <Grid row>
-              <h3>Check Result</h3>
+        <GridContainer className='padding-left-0 margin-left-0 padding-right-0'>
+            <Grid row>
+                <h3>Check Result</h3>
+            </Grid>
+            <Grid row>
+                <Grid col={4}>
+                    <Label test-id={"check-type-label"} htmlFor={"check-type"}>
+                        Check Type
+                    </Label>
+                    <Dropdown
+                        id={"check-type"}
+                        name={"check-type"}
+                        epa-testid={"check-type"}
+                        data-testid={"check-type"}
+                        value={selectedCheckType}
+                        onChange={onCheckTypeChange}
+                    >
+                        <option value={false}>{defaultDropdownText}</option>
+                        {checkTypeList.map((d) => <option key={d.checkTypeCode} value={d.checkTypeCode} data-testid={d.checkTypeCode}>{`${d.checkTypeDescription} (${d.checkTypeCode})`}</option>)}
+                    </Dropdown>
+                </Grid>
+                <Grid col={2}>
+                    <div className="margin-left-2">
+                        <Label test-id={"check-number-label"} htmlFor={"check-number"}>
+                            Check Number
+                        </Label>
+                        <Dropdown
+                            id={"check-number"}
+                            name={"check-number"}
+                            epa-testid={"check-number"}
+                            data-testid={"check-number"}
+                            value={selectedCheckNumber}
+                            onChange={onCheckNumberChange}
+                            disabled={!selectedCheckType}
+                        >
+                            <option value={false}>{defaultDropdownText}</option>
+                            {checkNumberList.map((d) => <option key={d} value={d} data-testid={d}>{d}</option>)}
+                        </Dropdown>
+                    </div>
+                </Grid>
+            </Grid>
+            <Grid row className="margin-top-2">
+                <Grid col={4}>
+                    <Label test-id={"check-result-label"} htmlFor={"check-result"}>
+                        Check Result
+                    </Label>
+                    <Dropdown
+                        id={"check-result"}
+                        name={"check-result"}
+                        epa-testid={"check-result"}
+                        data-testid={"check-result"}
+                        value={selectedCheckResult}
+                        onChange={(e) => setSelectedCheckResult(e.target.value)}
+                        disabled={!selectedCheckType || !selectedCheckNumber}
+                    >
+                        <option value={false}>{defaultDropdownText}</option>
+                        {checkResultList.map((d) => <option key={d} value={d} data-testid={d}>{d}</option>)}
+                    </Dropdown>
+                </Grid>
+            </Grid>
+          <Grid row className="margin-top-4">
+            <h3>Facility Location</h3>
+          </Grid>
+          <Grid row className="margin-top-4">
+            <h3>Facility Location</h3>
           </Grid>
           <Grid row>
-              <Grid col={4}>
-                  <Label test-id={"check-type-label"} htmlFor={"check-type"}>
-                      Check Type
-                  </Label>
-                  <Dropdown
-                      id={"check-type"}
-                      name={"check-type"}
-                      epa-testid={"check-type"}
-                      data-testid={"check-type"}
-                      value={selectedCheckType}
-                      onChange={onCheckTypeChange}
+            <Grid col={4}>
+              <Label test-id={"facility-name-label"} htmlFor={"facility-name"}>
+                Facility Name/ID
+              </Label>
+              <Dropdown
+                id={"facility-name"}
+                name={"facility-name"}
+                epa-testid={"facility-name"}
+                data-testid={"facility-name"}
+                value={selectedFacility}
+                onChange={onFacilityChange}
+              >
+                <option value={false}>{defaultDropdownText}</option>
+                {facilityList.map((d) => (
+                  <option
+                    key={d.orisCode}
+                    value={d.orisCode}
+                    data-testid={d.orisCode}
+                  >{`${d.facilityName} (${d.orisCode})`}</option>
+                ))}
+              </Dropdown>
+            </Grid>
+            <Grid col={4}>
+              <div className="margin-left-2">
+                <MultiSelectCombobox
+                  items={locationData}
+                  label="Location Name"
+                  entity="locationName"
+                  searchBy="contains"
+                  value={selectedLocations}
+                  onChangeUpdate={onChangeOfLocationMultiSelect}
+                  disabled={
+                    !(
+                      selectedCheckType &&
+                      selectedCheckNumber &&
+                      selectedCheckResult &&
+                      selectedFacility
+                    )
+                  }
+                ></MultiSelectCombobox>
+              </div>
+            </Grid>
+          </Grid>
+          <Grid row className="margin-top-4">
+            <Grid col={3}>
+              <h3>Active, Reason & Add Date</h3>
+            </Grid>
+            <Grid col={3}>
+              <Checkbox
+                id="is-active"
+                name="is-active"
+                label="Active"
+                className="margin-top-2"
+                value={selectedIsActive}
+              />
+            </Grid>
+          </Grid>
+          <Grid row>
+            <Grid col={4}>
+              <Label test-id={"reason-label"} htmlFor={"reason"}>
+                Reason
+              </Label>
+              <Dropdown
+                id={"reason"}
+                name={"reason"}
+                epa-testid={"reason"}
+                data-testid={"reason"}
+                value={selectedReason}
+                onChange={(e) => setSelectedReason(e.target.value)}
+              >
+                <option value={false}>{defaultDropdownText}</option>
+                {reasonCodeList.map((d) => (
+                  <option
+                    key={d.errorSuppressionReasonCode}
+                    value={d.errorSuppressionReasonCode}
+                    data-testid={d.errorSuppressionReasonCode}
                   >
-                      <option value={false}>{defaultDropdownText}</option>
-                      {checkTypeList.map((d) => <option key={d.checkTypeCode} value={d.checkTypeCode} data-testid={d.checkTypeCode}>{`${d.checkTypeDescription} (${d.checkTypeCode})`}</option>)}
-                  </Dropdown>
-              </Grid>
-              <Grid col={2}>
-                  <div className="margin-left-2">
-                      <Label test-id={"check-number-label"} htmlFor={"check-number"}>
-                          Check Number
-                      </Label>
-                      <Dropdown
-                          id={"check-number"}
-                          name={"check-number"}
-                          epa-testid={"check-number"}
-                          data-testid={"check-number"}
-                          value={selectedCheckNumber}
-                          onChange={onCheckNumberChange}
-                          disabled={!selectedCheckType}
-                      >
-                          <option value={false}>{defaultDropdownText}</option>
-                          {checkNumberList.map((d) => <option key={d} value={d} data-testid={d}>{d}</option>)}
-                      </Dropdown>
-                  </div>
-              </Grid>
+                    {d.errorSuppressionReasonCode}
+                  </option>
+                ))}
+              </Dropdown>
+            </Grid>
           </Grid>
           <Grid row className="margin-top-2">
-              <Grid col={4}>
-                  <Label test-id={"check-result-label"} htmlFor={"check-result"}>
-                      Check Result
-                  </Label>
-                  <Dropdown
-                      id={"check-result"}
-                      name={"check-result"}
-                      epa-testid={"check-result"}
-                      data-testid={"check-result"}
-                      value={selectedCheckResult}
-                      onChange={(e) => setSelectedCheckResult(e.target.value)}
-                      disabled={!selectedCheckType || !selectedCheckNumber}
-                  >
-                      <option value={false}>{defaultDropdownText}</option>
-                      {checkResultList.map((d) => <option key={d} value={d} data-testid={d}>{d}</option>)}
-                  </Dropdown>
-              </Grid>
-          </Grid>
-        <Grid row className="margin-top-4">
-          <h3>Facility Location</h3>
-        </Grid>
-        <Grid row className="margin-top-4">
-          <h3>Facility Location</h3>
-        </Grid>
-        <Grid row>
-          <Grid col={4}>
-            <Label test-id={"facility-name-label"} htmlFor={"facility-name"}>
-              Facility Name/ID
-            </Label>
-            <Dropdown
-              id={"facility-name"}
-              name={"facility-name"}
-              epa-testid={"facility-name"}
-              data-testid={"facility-name"}
-              value={selectedFacility}
-              onChange={onFacilityChange}
-            >
-              <option value={false}>{defaultDropdownText}</option>
-              {facilityList.map((d) => (
-                <option
-                  key={d.orisCode}
-                  value={d.orisCode}
-                  data-testid={d.orisCode}
-                >{`${d.facilityName} (${d.orisCode})`}</option>
-              ))}
-            </Dropdown>
-          </Grid>
-          <Grid col={4}>
-            <div className="margin-left-2">
-              <MultiSelectCombobox
-                items={locationData}
-                label="Location Name"
-                entity="locationName"
-                searchBy="contains"
-                value={selectedLocations}
-                onChangeUpdate={onChangeOfLocationMultiSelect}
-                disabled={
-                  !(
-                    selectedCheckType &&
-                    selectedCheckNumber &&
-                    selectedCheckResult &&
-                    selectedFacility
-                  )
-                }
-              ></MultiSelectCombobox>
-            </div>
-          </Grid>
-        </Grid>
-        <Grid row className="margin-top-4">
-          <Grid col={3}>
-            <h3>Active, Reason & Add Date</h3>
-          </Grid>
-          <Grid col={3}>
-            <Checkbox
-              id="is-active"
-              name="is-active"
-              label="Active"
-              className="margin-top-2"
-              value={selectedIsActive}
-            />
-          </Grid>
-        </Grid>
-        <Grid row>
-          <Grid col={4}>
-            <Label test-id={"reason-label"} htmlFor={"reason"}>
-              Reason
-            </Label>
-            <Dropdown
-              id={"reason"}
-              name={"reason"}
-              epa-testid={"reason"}
-              data-testid={"reason"}
-              value={selectedReason}
-              onChange={(e) => setSelectedReason(e.target.value)}
-            >
-              <option value={false}>{defaultDropdownText}</option>
-              {reasonCodeList.map((d) => (
-                <option
-                  key={d.errorSuppressionReasonCode}
-                  value={d.errorSuppressionReasonCode}
-                  data-testid={d.errorSuppressionReasonCode}
-                >
-                  {d.errorSuppressionReasonCode}
-                </option>
-              ))}
-            </Dropdown>
-          </Grid>
-        </Grid>
-        <Grid row className="margin-top-2">
-          <Grid col={3}>
-            <Label htmlFor="add-date-after" id="add-date-after-label">
-              Add Date After
-            </Label>
-            <DatePicker
-              aria-labelledby="add-date-after-label"
-              id="add-date-after"
-              name="add-date-after"
-              value={selectedAddDateAfter}
-            />
-          </Grid>
-          <Grid col={3}>
-            <div className="margin-left-4">
-              <Label htmlFor="add-date-before" id="add-date-before-label">
-                Add Date Before
+            <Grid col={3}>
+              <Label htmlFor="add-date-after" id="add-date-after-label">
+                Add Date After
               </Label>
               <DatePicker
-                aria-labelledby="add-date-before-label"
-                id="add-date-before"
-                name="add-date-before"
-                value={selectedAddDateBefore}
+                aria-labelledby="add-date-after-label"
+                id="add-date-after"
+                name="add-date-after"
+                value={selectedAddDateAfter}
               />
-            </div>
+            </Grid>
+            <Grid col={3}>
+              <div className="margin-left-4">
+                <Label htmlFor="add-date-before" id="add-date-before-label">
+                  Add Date Before
+                </Label>
+                <DatePicker
+                  aria-labelledby="add-date-before-label"
+                  id="add-date-before"
+                  name="add-date-before"
+                  value={selectedAddDateBefore}
+                />
+              </div>
+            </Grid>
+            <Grid col={4}>
+              <ButtonGroup type="default" className="float-right margin-top-3">
+                <Button type="button" className="usa-button usa-button--outline">
+                  Clear
+                </Button>
+                <Button type="button">Apply Filters</Button>
+              </ButtonGroup>
+            </Grid>
           </Grid>
-          <Grid col={4}>
-            <ButtonGroup type="default" className="float-right margin-top-3">
-              <Button type="button" className="usa-button usa-button--outline">
-                Clear
-              </Button>
-              <Button type="button">Apply Filters</Button>
-            </ButtonGroup>
-          </Grid>
-        </Grid>
-      </GridContainer>
+        </GridContainer>
     );
 }
