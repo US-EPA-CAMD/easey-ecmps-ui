68c104651e2dd18d2b8617814ed07b333aafccf0
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-02-07 14:54:01 -0500

    Filtering facilities by role

--- src/utils/api/facilityApi.js
+++ src/utils/api/facilityApi.js
@@ -1,21 +1,38 @@
 import axios from "axios";
 import { handleResponse, handleError } from "./apiUtils";
 import config from "../../config";
+import { secureAxios } from "./easeyAuthApi";
 
 axios.defaults.headers.common = {
   "x-api-key": config.app.apiKey,
 };
 
+//Endpoint that dynamically routes to workspace or non-workspace environments
+const fireApiGetRequest = async (path, workspaceOnly = false) => {
+  let url = config.services.facilities.uri;
+  let isWorkspace = false;
+
+  if (workspaceOnly || window.location.href.includes("/workspace")) {
+    url = `${url}/workspace`;
+    isWorkspace = true;
+  }
+
+  const payload = {
+    url: `${url}${path}`,
+    method: "GET",
+  };
+
+  if (isWorkspace) {
+    return secureAxios(payload).then(handleResponse).catch(handleError);
+  } else {
+    return axios(payload).then(handleResponse).catch(handleError);
+  }
+};
+
 export async function getAllFacilities() {
-  return axios
-    .get(`${config.services.facilities.uri}/facilities`)
-    .then(handleResponse)
-    .catch(handleError);
+  return await fireApiGetRequest("/facilities");
 }
 
 export async function getFacilityById(id) {
-  return axios
-    .get(`${config.services.facilities.uri}/facilities/${id}`)
-    .then(handleResponse)
-    .catch(handleError);
+  return await fireApiGetRequest(`/facilities/${id}`);
 }
