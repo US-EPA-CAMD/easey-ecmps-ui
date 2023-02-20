02e9a0075e2aab3dc6335f35858ed94e03376349
cw-cvp <106772898+cw-cvp@users.noreply.github.com>
2023-02-20 14:43:55 -0500

    Feature/4792 qa cert events tee export (#903)
    * format
    
    * add tables for qa cert and tee
    
    * Update ExportTab.js
    
    -rename qaTestSummaryData ref to rowsData
    -group selectedRows by dataKey: testSummary, qaCert, TEE
    
    * fix short circuit issue
    
    -evaluate checkedDataTypes[0].name after .length
    
    * Create assert-export.js
    
    * add export table columns for qa cert and tee
    
    * Update ExportTablesContainer.js
    
    -add tableTitle, dataKey props
    -set dataKey field in table rows to differentiate between test summary, qaCert, tee
    
    * group selected rows and ids by dataKey
    
    * moved grouping of selected rows to ExportTablesContainer
    
    -check all lists for test summary, qacert, tee to see if any selected
    
    * fix unit tests
    
    * unit tests
    
    * readd getReportingPeriod

--- src/utils/constants/tableColumns.js
+++ src/utils/constants/tableColumns.js
@@ -18,10 +18,10 @@ export const qaTestSummaryCols = [
 	{
 		name: "Test Number",
 		selector: (row) => row.testNumber,
-    sortable: true,
-    wrap: true,
-    compact: true,
-    minWidth: '120px'
+		sortable: true,
+		wrap: true,
+		compact: true,
+		minWidth: '120px'
 	},
 	{
 		name: "Test Reason Code",
@@ -39,11 +39,11 @@ export const qaTestSummaryCols = [
 			row.beginDate && row.beginHour && row.beginMinute
 				? `${row.beginDate} ${row.beginHour}:${row.beginMinute}`
 				: row.beginDate !== null && row.beginHour !== null
-				? `${row.beginDate} ${row.beginHour}`
-				: "",
-    sortable: true,
-    wrap: true,
-    compact: true,
+					? `${row.beginDate} ${row.beginHour}`
+					: "",
+		sortable: true,
+		wrap: true,
+		compact: true,
 	},
 	{
 		name: "End Date/Hr/Min",
@@ -51,10 +51,85 @@ export const qaTestSummaryCols = [
 			row.endDate && row.endHour && row.endMinute
 				? `${row.endDate} ${row.endHour}:${row.endMinute}`
 				: row.endDate !== null && row.endHour !== null
-				? `${row.endDate} ${row.endHour}`
-				: "",
-    sortable: true,
-    wrap: true,
-    compact: true,
+					? `${row.endDate} ${row.endHour}`
+					: "",
+		sortable: true,
+		wrap: true,
+		compact: true,
 	},
 ];
+
+export const qaCertificationEventDataCols = [
+	{
+		name: "Unit or StackPipe ID",
+		selector: (row) => (row.unitId ? row.unitId : row.stackPipeId),
+		sortable: true,
+	},
+	{
+		name: "System or Component ID",
+		selector: (row) =>
+			row.monitoringSystemID ? row.monitoringSystemID : row.componentID,
+		sortable: true,
+	},
+
+	{
+		name: "Event Code",
+		selector: (row) => row.qaCertEventCode,
+		sortable: true,
+	},
+	{
+		name: "Event Date",
+		selector: (row) => row.qaCertEventDate,
+		sortable: true,
+	},
+	{
+		name: "Event Hour",
+		selector: (row) => row.qaCertEventHour,
+		sortable: true,
+	},
+	{
+		name: "Required Test Code",
+		selector: (row) => row.requiredTestCode,
+		sortable: true,
+	},
+]
+
+export const qaTestExtensionExemptionDataCols = [
+	{
+		name: "Unit or StackPipe ID",
+		selector: (row) => (row.unitId ? row.unitId : row.stackPipeId),
+		sortable: true,
+	},
+	{
+		name: "System or Component ID",
+		selector: (row) =>
+			row.monitoringSystemID ? row.monitoringSystemID : row.componentID,
+		sortable: true,
+	},
+
+	{
+		name: "Submission Availability Code",
+		selector: (row) => row.submissionAvailabilityCode,
+		sortable: true,
+	},
+	{
+		name: "Pending Status Code",
+		selector: (row) => row.pendingStatusCode,
+		sortable: true,
+	},
+	{
+		name: "Eval Status Code",
+		selector: (row) => row.evalStatusCode,
+		sortable: true,
+	},
+	{
+		name: "fuelCode",
+		selector: (row) => row.fuelCode,
+		sortable: true,
+	},
+	{
+		name: "Extension or Exemption Code",
+		selector: (row) => row.extensionOrExemptionCode,
+		sortable: true,
+	},
+]
