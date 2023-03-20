eeb0d434ec247d56561b2d8615a7a1bc9e77631c
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-03-20 14:57:09 -0400

    remove .toString()

--- src/utils/selectors/monitoringPlanRectangularDucts.js
+++ src/utils/selectors/monitoringPlanRectangularDucts.js
@@ -5,12 +5,12 @@ export const getMonitoringPlansRectangularDuctsTableRecords = (totalData) => {
   const records = [];
 
   data.forEach((el) => {
-    const beginDate = formatDateString(el.wafBeginDate.toString())
+    const beginDate = formatDateString(el.wafBeginDate)
     const beginHour =
       el.wafBeginHour !== null ? el.wafBeginHour.toString() : "";
-    const endDate = formatDateString(el.wafEndDate.toString())
+    const endDate = formatDateString(el.wafEndDate)
     const endHour = el.wafEndHour !== null ? el.wafEndHour.toString() : "";
-    const wafDeterminationDate = formatDateString(el.wafDeterminationDate.toString())
+    const wafDeterminationDate = formatDateString(el.wafDeterminationDate)
     const wafMethodCode = el.wafMethodCode === "select" ? null : el.wafMethodCode;
 
     records.push({
