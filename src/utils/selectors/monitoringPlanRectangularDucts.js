1d6fd84be65c2112633fa6494c1f9cfcc1150685
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-03-20 14:31:48 -0400

    mp rect ducts format date

--- src/utils/selectors/monitoringPlanRectangularDucts.js
+++ src/utils/selectors/monitoringPlanRectangularDucts.js
@@ -1,36 +1,26 @@
+import { formatDateString } from "../functions";
+
 export const getMonitoringPlansRectangularDuctsTableRecords = (totalData) => {
   const data = totalData;
   const records = [];
 
   data.forEach((el) => {
-    const beginDate = el.wafBeginDate
-      ? formatStringToDate(el.wafBeginDate.toString())
-      : "";
+    const beginDate = formatDateString(el.wafBeginDate.toString())
     const beginHour =
       el.wafBeginHour !== null ? el.wafBeginHour.toString() : "";
-    const endDate = el.wafEndDate
-      ? formatStringToDate(el.wafEndDate.toString())
-      : "";
+    const endDate = formatDateString(el.wafEndDate.toString())
     const endHour = el.wafEndHour !== null ? el.wafEndHour.toString() : "";
-    const wafDeterminationDate = el.wafDeterminationDate
-      ? formatStringToDate(el.wafDeterminationDate.toString())
-      : "";
+    const wafDeterminationDate = formatDateString(el.wafDeterminationDate.toString())
     const wafMethodCode = el.wafMethodCode === "select" ? null : el.wafMethodCode;
 
     records.push({
       col1: wafDeterminationDate,
       col2: wafMethodCode,
-      col3: `${beginDate} ${beginHour}`,
-      col4: `${endDate} ${endHour}`,
+      col3: `${beginDate} ${beginHour}`.trim(),
+      col4: `${endDate} ${endHour}`.trim(),
       col5: el.id,
     });
   });
 
   return records;
-};
-// year - month - day to  month / day/ year
-const formatStringToDate = (date) => {
-  const parts = date.split("-");
-
-  return `${parts[1]}/${parts[2]}/${parts[0]}`;
 };
