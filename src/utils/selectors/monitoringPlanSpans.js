754ebb7816640ea346878c40a3431cdafb36dcb4
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-03-20 14:32:36 -0400

    mp spans format date

--- src/utils/selectors/monitoringPlanSpans.js
+++ src/utils/selectors/monitoringPlanSpans.js
@@ -1,29 +1,22 @@
+import { formatDateString } from "../functions";
+
 export const getMonitoringPlansSpansTableRecords = (totalData) => {
   const data = totalData;
   const records = [];
   data.forEach((el) => {
-    const beginDate = el.beginDate
-      ? formatStringToDate(el.beginDate.toString())
-      : "";
+    const beginDate = formatDateString(el.beginDate);
     const beginHour = el.beginHour !== null ? el.beginHour.toString() : "";
-    const endDate =
-      el.endDate !== null ? formatStringToDate(el.endDate.toString()) : "";
+    const endDate = formatDateString(el.endDate);
     const endHour = el.endHour !== null ? el.endHour.toString() : "";
     records.push({
       col1: el.componentTypeCode,
       col2: el.spanScaleCode,
       col3: el.spanMethodCode,
       col4: el.spanUnitsOfMeasureCode,
-      col5: `${beginDate} ${beginHour}`,
-      col6: `${endDate} ${endHour}`,
+      col5: `${beginDate} ${beginHour}`.trim(),
+      col6: `${endDate} ${endHour}`.trim(),
       col7: el.id,
     });
   });
   return records;
-};
-// year - month - day to  month / day/ year
-const formatStringToDate = (date) => {
-  const parts = date.split("-");
-
-  return `${parts[1]}/${parts[2]}/${parts[0]}`;
 };
