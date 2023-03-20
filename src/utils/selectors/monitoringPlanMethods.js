03a2d93eb36965d6e42114181caacf97a32ec68c
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-03-20 14:28:20 -0400

    mp methods date format

--- src/utils/selectors/monitoringPlanMethods.js
+++ src/utils/selectors/monitoringPlanMethods.js
@@ -1,55 +1,41 @@
+import { formatDateString } from "../functions";
+
 export const getMonitoringPlansMethodsTableRecords = (totalData) => {
   const data = totalData;
   const records = [];
 
   data.forEach((el) => {
-    const beginDate = el.beginDate
-      ? formatStringToDate(el.beginDate.toString())
-      : "";
+    const beginDate = formatDateString(el.beginDate);
     const beginHour = el.beginHour !== null ? el.beginHour.toString() : "";
-    const endDate = el.endDate ? formatStringToDate(el.endDate.toString()) : "";
+    const endDate = formatDateString(el.endDate);
     const endHour = el.endHour !== null ? el.endHour.toString() : "";
     records.push({
       col1: el.parameterCode,
       col2: el.monitoringMethodCode,
       col3: el.substituteDataCode,
       col4: el.bypassApproachCode,
-      col5: `${beginDate} ${beginHour}`,
-      col6: `${endDate} ${endHour}`,
+      col5: `${beginDate} ${beginHour}`.trim(),
+      col6: `${endDate} ${endHour}`.trim(),
       col7: el.id,
     });
   });
 
   return records;
 };
-// year - month - day to  month / day/ year
-const formatStringToDate = (date) => {
-  const parts = date.split("-");
-
-  return `${parts[1]}/${parts[2]}/${parts[0]}`;
-};
-
-export const formatAndNormalizeDate = (date) => {
-  const parts = date.split("-");
-
-  return `${parts[1]}/${parts[2].substring(0, 2)}/${parts[0]}`;
-};
 
 export const getMonitoringPlansMatsMethodsTableRecords = (data) => {
   const records = [];
 
   data.forEach((el) => {
-    const beginDate = el.beginDate
-      ? formatStringToDate(el.beginDate.toString())
-      : "";
+    const beginDate = formatDateString(el.beginDate);
     const beginHour = el.beginHour !== null ? el.beginHour.toString() : "";
-    const endDate = el.endDate ? formatStringToDate(el.endDate.toString()) : "";
+    const endDate = formatDateString(el.endDate);
     const endHour = el.endHour !== null ? el.endHour.toString() : "";
     records.push({
       col1: el.supplementalMATSParameterCode,
       col2: el.supplementalMATSMonitoringMethodCode,
-      col3: `${beginDate} ${beginHour}`,
-      col4: `${endDate} ${endHour}`,
+      col3: `${beginDate} ${beginHour}`.trim(),
+      col4: `${endDate} ${endHour}`.trim(),
       col5: el.id,
     });
   });
