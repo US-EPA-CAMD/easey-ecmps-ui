74cdc68d0e75b341ba6c9965a79c35e8548b7fb7
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-04-12 13:08:42 -0400

    getApiUrl for exportQA
    -add default empty string for getApiUrl

--- src/utils/api/qaCertificationsAPI.js
+++ src/utils/api/qaCertificationsAPI.js
@@ -4,7 +4,7 @@ import { handleResponse, handleError, handleImportError } from "./apiUtils";
 import config from "../../config";
 import { secureAxios } from "./easeyAuthApi";
 
-const getApiUrl = (path) => {
+const getApiUrl = (path = '') => {
   let url = config.services.qaCertification.uri;
 
   if (window.location.href.includes("/workspace")) {
@@ -210,12 +210,11 @@ export const exportQA = async (
     isHistoricalImport: Boolean,
   }
 ) => {
-  let url;
-
+  let url = getApiUrl()
   if (options.isOfficial) {
-    url = `${config.services.qaCertification.uri}/export?facilityId=${facilityId}`;
+    url = `${url}/export?facilityId=${facilityId}`
   } else {
-    url = getApiUrl(`/export?facilityId=${facilityId}`);
+    url = `${url}/export?facilityId=${facilityId}`
   }
 
   if (options.isHistoricalImport) {
