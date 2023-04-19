1be5c1e6d46db5c16d28f7cc76ecee9ced59739b
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-04-19 13:10:18 -0400

    remove options arg from validationErrors call

--- src/components/datatablesContainer/DataTableAssert/DataTableAssert.js
+++ src/components/datatablesContainer/DataTableAssert/DataTableAssert.js
@@ -280,11 +280,7 @@ export const DataTableAssert = ({
       ".modalUserInput",
       radioNames ? radioNames : null
     );
-    const validationErrors = validateUserInput(userInput, {
-      dataTableName,
-      lAttr,
-      rDat,
-    });
+    const validationErrors = validateUserInput(userInput, dataTableName);
     if (validationErrors.length > 0) {
       setErrorMsgs(validationErrors);
       return;
