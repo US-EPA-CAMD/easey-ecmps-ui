704f9ecf3365e73f005067a093c806589e5e63e7
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-04-20 15:53:41 -0400

    remove unused import

--- src/components/datatablesContainer/DataTableSystems/DataTableSystems.js
+++ src/components/datatablesContainer/DataTableSystems/DataTableSystems.js
@@ -7,9 +7,6 @@ import { DataTableRender } from "../../DataTableRender/DataTableRender";
 import * as mpApi from "../../../utils/api/monitoringPlansApi";
 import ModalDetails from "../../ModalDetails/ModalDetails";
 import { extractUserInput, validateUserInput } from "../../../additional-functions/extract-user-input";
-import {
-  needEndDate,
-} from "../../../additional-functions/app-error";
 import { Preloader } from "@us-epa-camd/easey-design-system";
 import { connect } from "react-redux";
 import { loadDropdowns } from "../../../store/actions/dropdowns";
