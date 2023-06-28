f93343d0960db79ae2b5e55e6f1b9bcc469c617e
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-06-28 10:53:17 -0400

    fix merge conflicts

--- src/components/AdminMaintenance/EmSubmissionData/EmSubmissionData.js
+++ src/components/AdminMaintenance/EmSubmissionData/EmSubmissionData.js
@@ -6,6 +6,9 @@ import { ArrowDownwardSharp } from '@material-ui/icons';
 import { submissionAccessTitle } from '../../../utils/constants/moduleTitles';
 import { EmSubmissionModal } from '../EmSubmissionPopOut/EmSubmissionPopout'
 import "./EmSubmissionData.scss";
+import Modal from '../../Modal/Modal';
+import ModalDetails from '../../ModalDetails/ModalDetails';
+import { modalViewData } from '../../../additional-functions/create-modal-input-controls';
 
 export const EmSubmissionData = ({
     data = [],
@@ -19,6 +22,60 @@ export const EmSubmissionData = ({
     const [showApproveModal, setShowApproveModal] = useState(false);
     // This array contains the rows that are selected in the table. Use this to do logic to disable/enable buttons
     const [selectedRows, setSelectedRows] = useState([]);
+
+    const [selectedModalData, setSelectedModalData] = useState(null);
+    const [selectedRowData, setSelectedRowData] = useState(null)
+
+    // TODO, temp remove later
+    // check out qa expandable rows render to see how modals and view buttons are displayed
+    // maybe make a E
+    // qa expandabla rows uses qa data table render which has cell: (row, index) => return element to render
+    data = [
+        {
+            emissionStatusCode: "emissionStatusCode",
+            submissionAvailabilityCode: "submissionAvailabilityCode",
+            resubExplanation: "resubExplanation",
+            closeDate: "2023-06-28T14:17:05.648Z",
+            openDate: "2023-06-28T14:17:05.648Z",
+            monitorPlanId: "monitorPlanId",
+            reportingPeriodId: 1,
+            id: 2,
+            facilityId: 3,
+            orisCode: 4,
+            state: "state",
+            locations: "locations",
+            reportingFrequencyCode: "reportingFrequencyCode",
+            submissionTypeCode: "submissionTypeCode",
+            status: "status",
+            lastSubmissionId: 5,
+            severityLevel: "severityLevel",
+            userid: "userid",
+            addDate: "2023-06-28T14:17:05.648Z",
+            updateDate: "2023-06-28T14:17:05.648Z"
+        },
+        {
+            emissionStatusCode: "emissionStatusCode2",
+            submissionAvailabilityCode: "submissionAvailabilityCode2",
+            resubExplanation: "resubExplanation2",
+            closeDate: "2023-06-28T14:17:05.648Z",
+            openDate: "2023-06-28T14:17:05.648Z",
+            monitorPlanId: "monitorPlanId2",
+            reportingPeriodId: 11,
+            id: 22,
+            facilityId: 33,
+            orisCode: 44,
+            state: "state2",
+            locations: "locations2",
+            reportingFrequencyCode: "reportingFrequencyCode2",
+            submissionTypeCode: "submissionTypeCode2",
+            status: "status2",
+            lastSubmissionId: 55,
+            severityLevel: "severityLevel2",
+            userid: "userid2",
+            addDate: "2023-06-28T14:17:05.648Z",
+            updateDate: "2023-06-28T14:17:05.648Z"
+        }
+    ]
 
     const getSelect = useCallback((row, idx) => {
         return (
@@ -36,6 +93,60 @@ export const EmSubmissionData = ({
             </div>
         );
     }, []);
+
+    const openModalHandler = (row, index, isCreate) => {
+        console.log({ row, index });
+        let selectedData = null;
+
+        selectedData = data[1]
+
+        setSelectedRowData(data[0])
+
+        // const controlInputs = {
+        //     parameterCode: ["Parameter", "mainDropdown", ""],
+        //     name: ["Name", "input", ""],
+        //     email: ["Email", "input", ""],
+        //     bypassApproachCode: ["Bypass Approach", "dropdown", ""],
+        // }
+
+        // const controlDatePickerInputs = {
+        //     beginDate: ["Start Date", "date", ""],
+        //     beginHour: ["Start Time", "hourDropdown", ""],
+        //     endDate: ["End Date", "date", ""],
+        //     endHour: ["End Time", "hourDropdown", ""],
+        // }
+        const controlDatePickerInputs = null
+
+        const mdmData = {}
+
+        const prefilteredDataName = false
+
+        const mainDropdownName = null
+        const mainDropdownResult = []
+        const prefilteredTotalName = 'prefilteredTotalName'
+        const extraControls = false
+
+        setSelectedModalData(
+            modalViewData(
+                selectedData,
+                controlInputs,
+                controlDatePickerInputs,
+                isCreate,
+                mdmData,
+                prefilteredDataName ? mdmData[prefilteredDataName] : "",
+                mainDropdownName,
+                mainDropdownResult,
+                true,
+                prefilteredTotalName,
+                extraControls
+            )
+        )
+        // setShowModal(true);
+    }
+
+    const closeModalHandler = () => {
+        // setShowModal(false);
+    }
 
     const onRowSelection = (row, checked) => {
         row.selected = checked;
@@ -201,9 +312,8 @@ export const EmSubmissionData = ({
                     </div>
                 </div>
                 <div className="es-datatable margin-top-5">
-                    {isLoading ? (
-                        <Preloader />
-                    ) : (
+                    {isLoading && <Preloader />}
+                    {!isLoading &&
                         <DataTable
                             sortIcon={
                                 <ArrowDownwardSharp className="margin-left-2 text-primary" />
@@ -215,9 +325,47 @@ export const EmSubmissionData = ({
                             data={data}
                             className={`data-display-table react-transition fade-in`}
                         />
-                    )}
+                    }
                 </div>
             </div>
+            {false &&
+                <Modal
+                    show={() => { }}
+                    close={closeModalHandler}
+                >
+                    <ModalDetails
+                        modalData={selectedRowData}
+                        data={selectedModalData}
+                        cols={3}
+                        title={'TEMP em submission data title'}
+                        // viewOnly={!user || (user && !isCheckedOut)}
+                        viewOnly={true}
+                    // create={createNewData}
+                    />
+                </Modal>
+            }
         </div>
     )
+}
+
+const controlInputs = {
+    // cols usese .orisCode for fac name/id
+    facilityId: ["Facility Name/ID", "input", ""],
+    state: ["State", "input", ""],
+    locations: ["MP Location(s)", "input", ""],
+    reportingPeriodId: ["Reporting Period", "input", ""],
+    reportingFrequencyCode: ["Reporting Frequency", "input", ""],
+    status: ["Status", "input", ""],
+    lastWindowField: ["Last Window", "input", ""],
+    openDate: ["Open Date", "date", ""],
+    closeDate: ["Close Date", "date", ""],
+    emissionStatusCode: ["Emission Status", "input", ""],
+    submissionAvailabilityCode: ["Submission Availability", "input", ""],
+    submissionStatusField: ["Submission Status", "input", ""],
+    lastSubmissionId: ["Last Submission ID", "input", ""],
+    submissionDateTimeField: ["Submission Date/Time", "input", ""],
+    submissionTypeCode: ["Submission Type", "input", ""],
+    severityLevel: ["Severity Level", "input", ""],
+    representativeField: ["Representative", "input", ""],
+    analystField: ["Analyst", "input", ""],
 }
