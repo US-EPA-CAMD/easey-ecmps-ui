974f2a973204583874a835515885dc17a627cc42
Quang Bui <quangbui@cvpcorp.com>
2023-07-24 14:33:59 -0400

    cleaned up

--- src/utils/selectors/monitoringPlanCPMSQualifications.js
+++ src/utils/selectors/monitoringPlanCPMSQualifications.js
@@ -1,12 +1,10 @@
-import { validateDate } from "../functions";
-
 export const getMonitoringPlansCPMSQualifications = (totalData) => {
   const data = totalData;
   const records = [];
 
   data.forEach((el) => {
     records.push({
-      col1: validateDate(el.qualificationTestDate),
+      col1: el.qualificationTestDate,
       col2: el.stackTestNumber ? el.stackTestNumber : "",
       col3: el.operatingLimit ? el.operatingLimit : "",
 
