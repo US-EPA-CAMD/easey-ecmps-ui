32d5b3007de94e87765fd971ead07745a3123551
cw-cvp <106772898+cw-cvp@users.noreply.github.com>
2023-08-08 12:49:31 -0400

    Feature/5512 mats bulk file modal (#1214)
    * add mats modal popup
    
    * add mats file option
    
    * mats modal content
    
    ---------
    
    Co-authored-by: Quang Bui <68402491+Qb10@users.noreply.github.com>

---
+++ src/components/ImportModal/ImportModalMatsContent/ImportModalMatsContent.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import DropdownSelection from "../../DropdownSelection/DropdownSelection";
+import { FileInput, FormGroup, Label } from "@trussworks/react-uswds";
+import { getQATestSummary } from "../../../utils/api/qaCertificationsAPI";
+
+const initialSelectOption = { key: "select", name: "--- Select a value ---" }
+
+const ImportModalMatsContent = ({
+  locationId
+}) => {
+  const [selection, setSelection] = useState(0);
+  const [testNums, setTestNums] = useState([initialSelectOption])
+
+  useEffect(() => {
+    const fetchTestNumbers = async () => {
+      try {
+        const resp = await getQATestSummary(locationId)
+
+        const testNums = resp.data.map(testSummary => {
+          const { testNumber } = testSummary
+          return { key: testNumber, name: testNumber }
+        })
+        setTestNums([initialSelectOption, ...testNums])
+      } catch (e) {
+        console.log('error fetching test numbers', e)
+      }
+    }
+    fetchTestNumbers()
+  }, [locationId])
+
+  const onChangeHandler = (e) => {
+    console.log(e.target.files);
+  };
+
+  return (
+    <div>
+      <DropdownSelection
+        caption={"Test Number"}
+        options={testNums}
+        viewKey={"name"}
+        selectKey={"key"}
+        initialSelection={selection}
+        selectionHandler={(value) => setSelection(value[0])}
+      />
+      <FormGroup>
+        <Label htmlFor="file-input-multiple">File Upload</Label>
+        <FileInput
+          id="file-input-multiple"
+          name="file-input-multiple"
+          multiple
+          onChange={onChangeHandler}
+        />
+      </FormGroup>
+    </div>
+  )
+}
+
+export default ImportModalMatsContent
