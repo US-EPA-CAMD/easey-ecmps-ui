fe8067896f29040bd215031b86736e2d16791f17
Corey W <106772898+cw-cvp@users.noreply.github.com>
2023-08-31 15:55:41 -0400

    display error if either exportEmissions or importEmissions data fails

--- src/components/HeaderInfo/ImportHistoricalDataModal.js
+++ src/components/HeaderInfo/ImportHistoricalDataModal.js
@@ -6,6 +6,7 @@ import {
   importEmissionsData,
 } from "../../utils/api/emissionsApi";
 import { useSelector } from "react-redux";
+import { successResponses } from "../../utils/api/apiUtils";
 
 export const ImportHistoricalDataModal = ({
   closeModalHandler,
@@ -25,43 +26,34 @@ export const ImportHistoricalDataModal = ({
         ?.selectedConfig
   );
 
-  const historicalImport = () => {
+  const historicalImport = async () => {
     if (selectedYear === null || selectedQuarter === null || !selectedConfig)
       return;
 
     setIsLoading(true);
     setFinishedLoading(false);
 
-    exportEmissionsData(selectedConfig.id, selectedYear, selectedQuarter, false)
-      .then(({ data: exportResponse }) => {
-        return importEmissionsData(exportResponse);
-      })
-      .then(({ data: importResponse, status }) => {
-        
-        if (status === 201) {
-          setImportedFileErrorMsgs([]);
-          portCallback(selectedYear, selectedQuarter)
-        } else if (importResponse?.message) {
-          if(Array.isArray(importResponse.message)) {
-            setImportedFileErrorMsgs(
-              importResponse?.message || [`HTTP ${status} Error`]
-            );
-          } else {
-            setImportedFileErrorMsgs(
-              importResponse?.message?.split(",") || [`HTTP ${status} Error`]
-            );
-          }
-        } else {
-          setImportedFileErrorMsgs([`HTTP ${status} Error`]);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        setIsLoading(false);
-        setFinishedLoading(true);
-      });
+    try {
+      const exportResp = await exportEmissionsData(selectedConfig.id, selectedYear, selectedQuarter, false)
+      if (!successResponses.includes(exportResp.status)) {
+        throw exportResp // go to catch block
+      }
+
+      const importResp = await importEmissionsData(exportResp.data);
+      console.log('import resp', importResp);
+      if (!successResponses.includes(importResp.status)) {
+        throw importResp // go to catch block
+      }
+
+      setImportedFileErrorMsgs([]);
+      portCallback(selectedYear, selectedQuarter)
+    } catch (error) {
+      const errorMsgs = error?.data?.message ?? ['There was an error importing historical data']
+      setImportedFileErrorMsgs(errorMsgs)
+    } finally {
+      setIsLoading(false);
+      setFinishedLoading(true);
+    }
   };
 
   const setYearAndQuarter = (reportingPeriodObj) => {
