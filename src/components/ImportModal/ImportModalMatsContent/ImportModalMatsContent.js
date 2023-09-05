4bee788d0750ae440143112339ff510414950bc4
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-09-05 19:06:34 -0400

    Change to upload of mats files to include test group and location

--- src/components/ImportModal/ImportModalMatsContent/ImportModalMatsContent.js
+++ src/components/ImportModal/ImportModalMatsContent/ImportModalMatsContent.js
@@ -1,59 +1,154 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { FormGroup, Label } from "@trussworks/react-uswds";
 
 import DropdownSelection from "../../DropdownSelection/DropdownSelection";
 import { FileInput } from "../../FileInput/FileInput";
-import { getQATestSummary, getQATestSummaryOfficial } from "../../../utils/api/qaCertificationsAPI";
+import { getQATestSummary } from "../../../utils/api/qaCertificationsAPI";
 
-const initialSelectOption = { key: "select", name: "--- Select a value ---" }
+const initialStateLocations = {
+  id: "select-0",
+  name: "--- Select a value ---",
+};
+const initialStateTypes = { key: "select-1", name: "--- Select a value ---" };
+const initialStateNums = { key: "select-2", name: "--- Select a value ---" };
 
 const ImportModalMatsContent = ({
-  locationId,
   setImportedFile,
+  importedFile,
   selectedTestNumberRef,
+  testCodeLegend,
+  locations,
+  setDisablePortBtn,
 }) => {
-  const [selection, setSelection] = useState(0);
-  const [testNums, setTestNums] = useState([initialSelectOption])
+  const testSummaryRecords = useRef([]);
 
-  useEffect(() => {
-    const fetchTestNumbers = async () => {
+  const [locationSelection, setLocationSelection] = useState(0);
+  const [testGroupSelection, setTestGroupSelection] = useState(0);
+  const [testNumberSelection, setTestNumberSelection] = useState(0);
+
+  const [locationsDropdown] = useState([initialStateLocations, ...locations]);
+  const [testTypes, setTestTypes] = useState([initialStateTypes]);
+  const [testNums, setTestNums] = useState([initialStateNums]);
+
+  const updateLocationSelection = async (value) => {
+    setDisablePortBtn(true);
+    setLocationSelection(value[0]);
+
+    setTestTypes([initialStateTypes]);
+    setTestGroupSelection(0); //On new selection reset the previous selections
+
+    setTestNums([initialStateNums]);
+    setTestNumberSelection(0);
+
+    if (value[0] !== 0) {
       try {
-        const respWorkspace = await getQATestSummary(locationId)
-        const respOfficial = await getQATestSummaryOfficial(locationId)
+        const resp = await getQATestSummary(value[1]);
+        testSummaryRecords.current = resp.data; //Store these for later us when filtering by test type as well
 
-        const allTestSummaries = [...respWorkspace.data, ...respOfficial.data]
-        const testNums = allTestSummaries.map(testSummary => testSummary.testNumber)
-        const testNumsSet = new Set(testNums) // remove duplicates
-        const testNumsData = Array.from(testNumsSet).sort().map(testNum => ({key: testNum, name: testNum}))
+        const groupCodes = Array.from(
+          new Set(testSummaryRecords.current.map((ts) => ts.testTypeCode))
+        ); //Extract group codes and remove duplicates
 
-        setTestNums([initialSelectOption, ...testNumsData])
+        const testTypeOptions = [initialStateTypes];
+
+        groupCodes.forEach((cd) => {
+          testTypeOptions.push({
+            key: cd,
+            name: testCodeLegend.find((f) => f.testTypeCode === cd)
+              .testTypeDescription,
+          });
+        });
+
+        setTestTypes(testTypeOptions);
       } catch (e) {
-        console.log('error fetching test numbers', e)
+        console.log("error fetching test numbers", e);
       }
     }
-    fetchTestNumbers()
-  }, [locationId])
+  };
 
-  const selectTestNumberHandler = (value) => {
-    setSelection(value[0])
-    selectedTestNumberRef.current = value[0]
-  }
+  const updateTypeSelection = async (value) => {
+    setDisablePortBtn(true);
+    setTestGroupSelection(value[0]); //On new selection reset the previous selections
+
+    setTestNums([initialStateNums]);
+    setTestNumberSelection(0);
+
+    if (value[0] !== 0) {
+      const testSummaryFiltered = testSummaryRecords.current.filter(
+        (f) => f.testTypeCode === value[1]
+      );
+
+      const options = testSummaryFiltered.map((m) => ({
+        key: m.testNumber,
+        name: m.testNumber,
+      }));
+
+      setTestNums([initialStateNums, ...options]);
+    }
+  };
+
+  const updateNumSelection = async (value) => {
+    setTestNumberSelection(value[0]); //On new selection reset the previous selections
+
+    selectedTestNumberRef.current = {
+      location: locationsDropdown[locationSelection].name,
+      testTypeGroup: testTypes[testGroupSelection].key,
+      testNumber: testNums[value[0]].key,
+    };
+
+    console.log(selectedTestNumberRef.current);
+
+    if (value[0] !== 0 && importedFile.length !== 0) {
+      setDisablePortBtn(false);
+    } else {
+      setDisablePortBtn(true);
+    }
+  };
 
   const onChangeHandler = (e) => {
-    console.log(e.target.files);
-    setImportedFile(e.target.files)
+    setImportedFile(e.target.files);
+
+    if (testNumberSelection !== 0 && e.target.files.length > 0) {
+      setDisablePortBtn(false);
+    } else {
+      setDisablePortBtn(true);
+    }
   };
 
   return (
     <div>
-      <DropdownSelection
-        caption={"Test Number"}
-        options={testNums}
-        viewKey={"name"}
-        selectKey={"key"}
-        initialSelection={selection}
-        selectionHandler={selectTestNumberHandler}
-      />
+      <div className="grid-row">
+        <div className="grid-col-4">
+          <DropdownSelection
+            caption={"Location"}
+            options={locationsDropdown}
+            viewKey="name"
+            selectKey="id"
+            initialSelection={locationSelection}
+            selectionHandler={updateLocationSelection}
+          />
+        </div>
+        <div className="grid-col-8">
+          <DropdownSelection
+            caption={"Test Type Group"}
+            options={testTypes}
+            viewKey={"name"}
+            selectKey={"key"}
+            initialSelection={testGroupSelection}
+            selectionHandler={updateTypeSelection}
+          />
+        </div>
+        <div className="grid-col-4">
+          <DropdownSelection
+            caption={"Test Number"}
+            options={testNums}
+            viewKey={"name"}
+            selectKey={"key"}
+            initialSelection={testNumberSelection}
+            selectionHandler={updateNumSelection}
+          />
+        </div>
+      </div>
       <FormGroup>
         <Label htmlFor="file-input-multiple">File Upload</Label>
         <FileInput
@@ -64,7 +159,7 @@ const ImportModalMatsContent = ({
         />
       </FormGroup>
     </div>
-  )
-}
+  );
+};
 
-export default ImportModalMatsContent
+export default ImportModalMatsContent;
