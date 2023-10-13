9b9a9f25ed7ef0130cbf173c1684bc274bf8d933
Quang Bui <68402491+Qb10@users.noreply.github.com>
2023-10-13 12:56:38 -0400

    Feature/5746 config view (#1318)
    * fixed config data table
    
    * added hover
    
    ---------
    
    Co-authored-by: Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>

--- src/components/DataTableRender/DataTableRender.js
+++ src/components/DataTableRender/DataTableRender.js
@@ -214,7 +214,21 @@ export const DataTableRender = ({
           wrap: true,
           selector: (row) => row[`col${index + 1}`],
           sortable: true,
-          cell: (row) => <AddPencil row={row} />,
+          width: "20%", // Set the column width to 30%
+          cell: (row) => (
+            <div
+              className="usa-tooltip"
+              title={row[`col${index + 1}`]}
+              style={{
+                width: "100%",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }}
+            >
+              <AddPencil row={row} />
+            </div>
+          ),
         });
         break;
 
@@ -234,6 +248,7 @@ export const DataTableRender = ({
         columns.push({
           id: `col${index + 1}`,
           name,
+          width: "20%", // Set the column width to 30%
           selector: (row) => row[`col${index + 1}`],
           sortable: true,
           style: { whiteSpace: "normal" },
@@ -501,7 +516,7 @@ export const DataTableRender = ({
         />
       );
     }
-    
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [workspaceSection]);
 
@@ -533,11 +548,13 @@ export const DataTableRender = ({
               keyField={!uniqueKey ? `col${columnNames.length + 1}` : "col1"}
               className={`data-display-table react-transition fade-in`}
               sortIcon={
-                hasSortIcon ? <ArrowDownwardSharp className="margin-left-2 text-primary" /> : null
+                hasSortIcon ? (
+                  <ArrowDownwardSharp className="margin-left-2 text-primary" />
+                ) : null
               }
               // props
               defaultSortFieldId={defaultSort ? defaultSort : "col1"}
-              defaultSortAsc={defaultSortDir === 'asc'}
+              defaultSortAsc={defaultSortDir === "asc"}
               expandableRows={expandableRows}
               pagination={pagination}
               columns={columns}
@@ -599,10 +616,11 @@ export const DataTableRender = ({
           <div>
             {tableTitle ? (
               <h4
-                className={`margin-top-5 text-bold ${tableStyling
+                className={`margin-top-5 text-bold ${
+                  tableStyling
                     ? "mobile:font-body-md mobile:text-bold"
                     : "mobile:font-body-xl mobile:text-bold"
-                  }`}
+                }`}
               >
                 {tableTitle}
               </h4>
@@ -626,7 +644,8 @@ export const DataTableRender = ({
                         addBtn(false, false, true);
                       }}
                       id={
-                        addBtnName.toLowerCase().split(" ").join("-") + "-add-btn"
+                        addBtnName.toLowerCase().split(" ").join("-") +
+                        "-add-btn"
                       }
                     >
                       {addBtnName}
