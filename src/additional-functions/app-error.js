eecd53df8624669e667d38f465d670a3168ef871
Fahim Anwari <fahimanwari@cvpcorp.com>
2023-10-16 23:02:06 -0400

    Removed commented code

--- src/additional-functions/app-error.js
+++ src/additional-functions/app-error.js
@@ -1,49 +1,11 @@
 export const needEndDate = "Must enter in both End Date and End Time";
 
-// export const displayAppError = (error) => {
-//   const formattedError = Array.isArray(error) ? error.join('\n') : error
-//   const appearAnimation = "flip-in-x-reverse";
-
-//   if (typeof document !== 'undefined' && document.querySelector("#appErrorMessageText")) {
-//     document.querySelector("#appErrorMessageText").innerHTML = formattedError;
-
-//     const errorMsg = document.querySelector("#appErrorMessage")
-
-//     errorMsg.classList.remove("display-none");
-//     errorMsg.classList.remove(appearAnimation);
-//     errorMsg.classList.add(appearAnimation);
-//     document
-//       .querySelectorAll("svg path")
-//       .forEach((element) => (element.tabIndex = -1));
-
-//     errorMsg.addEventListener("click", hideAppError);
-
-//     const svgElements = document.querySelectorAll('#appErrorMessage .MuiSvgIcon-root');
-//     const xButtonSvg = svgElements[1];
-//     if (xButtonSvg) {
-//       xButtonSvg.setAttribute('aria-label', 'Close error message');
-//        }
-//   }
-// };
-
-// export const hideAppError = () => {
-//   if (typeof document !== 'undefined' && document.querySelector("#appErrorMessageText")) {
-//     document.querySelector("#appErrorMessageText").innerHTML = "";
-//     document.querySelector("#appErrorMessage").classList.add("display-none");
-//     window.removeEventListener("click", hideAppError);
-//   }
-// };
-
-
 const displayAppMessage = (messages, type, hideFunc) => {
   const formattedError = Array.isArray(messages) ? messages.join('\n') : messages
   const appearAnimation = "flip-in-x-reverse";
 
   const containerId = `#app${type}Message`;
   const textMessageContainerId = `#app${type}MessageText`
-
-  console.log(containerId)
-  console.log(textMessageContainerId)
 
   if (typeof document !== 'undefined' && document.querySelector(textMessageContainerId)) {
     document.querySelector(textMessageContainerId).innerHTML = formattedError;
