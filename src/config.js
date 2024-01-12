2b64cc4c51c05900b42820019ee665eff7f02078
shahbazkhan1999 <shahbazkhaan1999@gmail.com>
2024-01-12 09:33:37 -0800

    The Sponsor role or Initial Authorizer need to have the ability to submit- UI

--- src/config.js
+++ src/config.js
@@ -112,6 +112,7 @@ export const config = {
     preparerRole: getConfigValue("EASEY_AUTH_API_PREPARER_ROLE", "Preparer"),
     analystRole: getConfigValue("EASEY_AUTH_API_ANALYST_ROLE", "ECMPS Analyst"),
     adminRole: getConfigValue("EASEY_AUTH_API_ADMIN_ROLE", "ECMPS Admin"),
+    initialAuthorizerRole: getConfigValue("EASEY_AUTH_API_INITIAL_AUTHORIZER_ROLE", "Initial Authorizer"),
   },
   services: {
     mdm: {
