9a97704c81d408bfe0e92e6893c326a37313bddf
ergjustin <justin.joseph.erg.com>
2024-11-04 02:04:17 -0600

    feat:added client token in clienttokenAxiox

--- src/utils/api/clientTokenAxios.js
+++ src/utils/api/clientTokenAxios.js
@@ -17,6 +17,7 @@ export const clientTokenAxios = async (options) => {
     authorization: `Bearer ${localStorage.getItem("client_token")}`,
     "x-api-key": config.app.apiKey,
     "x-client-id": config.app.clientId,
+    "x-client-token": `Bearer ${localStorage.getItem("client_token")}`
   };
 
   return axios(options);
