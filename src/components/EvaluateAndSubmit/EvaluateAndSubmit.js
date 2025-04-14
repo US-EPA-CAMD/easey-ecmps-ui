69e65d2554215510376d2605a8fa4e200eed4a3f
spetros-do <153679433+spetros-do@users.noreply.github.com>
2025-04-14 04:23:11 -0700

    Cleaned up code & Removed hasCritErrors flag from UI payload in EvaluateAndSubmit.js as part of the effort for #6629

--- src/components/EvaluateAndSubmit/EvaluateAndSubmit.js
+++ src/components/EvaluateAndSubmit/EvaluateAndSubmit.js
@@ -411,7 +411,6 @@ export const EvaluateAndSubmit = ({
     payload.userEmail = user.email;
 
     payload.items = [];
-    //payload.hasCritErrors = false;
     for (const monPlanId of activeMPSet) {
       const newItem = {};
       newItem.monPlanId = monPlanId;
@@ -426,10 +425,6 @@ export const EvaluateAndSubmit = ({
           (f) => f.monPlanId === monPlanId && f.isSelected
         );
 
-       /**if (plan.evalStatusCode === "ERR") {
-          payload.hasCritErrors = true;
-        }**/
-
         newItem.submitMonPlan = true;
       } else {
         newItem.submitMonPlan = false;
@@ -439,27 +434,18 @@ export const EvaluateAndSubmit = ({
       newItem.testSumIds = qaTestSumRef.current
         .filter((f) => f.monPlanId === monPlanId && f.isSelected)
         .map((m) => {
-          /**if (m.evalStatusCode === "ERR") {
-            payload.hasCritErrors = true;
-          }**/
 
           return m.testSumId;
         });
       newItem.qceIds = qaCertEventRef.current
         .filter((f) => f.monPlanId === monPlanId && f.isSelected)
         .map((m) => {
-          /**if (m.evalStatusCode === "ERR") {
-            payload.hasCritErrors = true;
-          }**/
 
           return m.qaCertEventIdentifier;
         });
       newItem.teeIds = qaTeeRef.current
         .filter((f) => f.monPlanId === monPlanId && f.isSelected)
         .map((m) => {
-          /**if (m.evalStatusCode === "ERR") {
-            payload.hasCritErrors = true;
-          }**/
 
           return m.testExtensionExemptionIdentifier;
         });
@@ -468,9 +454,7 @@ export const EvaluateAndSubmit = ({
       newItem.emissionsReportingPeriods = emissionsRef.current
         .filter((f) => f.monPlanId === monPlanId && f.isSelected)
         .map((m) => {
-          /**if (m.evalStatusCode === "ERR") {
-            payload.hasCritErrors = true;
-          }**/
+
           return m.periodAbbreviation;
         });
 
