64b589e96aaf93802c39a761b54eb80166f246b2
spetros-do <153679433+spetros-do@users.noreply.github.com>
2025-04-15 08:47:59 -0700

    Updated code to allow MP to begin dates up to 90 days in the future.

--- src/components/ModalDetails/ModalDetails.js
+++ src/components/ModalDetails/ModalDetails.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-unused-vars, no-prototype-builtins, react/no-unknown-property, no-case-declarations */
+// TODO: Address ESLint issues in a separate ticket
+// - Remove unused 'prefilteredMdmData' prop
+// - Replace direct hasOwnProperty calls with Object.prototype.hasOwnProperty.call
+// - Replace custom 'epadataname' attributes with standard data-* attributes
+// - Fix case block declarations by adding block scopes
+
 import React, { useEffect, useState } from "react";
 import {
   Label,
@@ -376,7 +383,16 @@ const ModalDetails = ({
             defaultValue={datePickerValue}
             disabled={isEditingDisabled}
             maxDate={
-              title !== "Protocol Gas"
+               // Allow future dates up to 90 days for specific components
+              ["Default", "Formula", "Load", "Location Attribute", "Method",
+                "Qualification", "Rectangular Duct WAF", "Span", "System",
+                "Unit Information", "Unit Fuel", "Unit Control", "Unit Capacity"].includes(title)
+                ? (() => {
+                    const futureDate = new Date(d.getTime() + (90 * 24 * 60 * 60 * 1000));
+                    return `${futureDate.getFullYear()}-${futureDate.getMonth() + 1}-${futureDate.getDate()}`;
+                  })()
+
+              : title !== "Protocol Gas"
                 ? `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
                 : null
             }
