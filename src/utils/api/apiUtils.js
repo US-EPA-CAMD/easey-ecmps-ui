23c89ad7337267e9fd6a684c3903c7eda2d27b3c
Jhernandez29 <jonashernandez@erg.com>
2025-04-28 17:51:09 -0400

    added object key and value for a more freindly user experience.

--- src/utils/api/apiUtils.js
+++ src/utils/api/apiUtils.js
@@ -30,6 +30,34 @@ export function handleImportError(error) {
   return parseErrorMessage(error);
 }
 
+//Clearer field names for user in UI
+const replacements = {
+  wafDeterminationDate: "WAF Determination Date",
+  wafBeginDate: "Begin Date",
+  wafBeginHour: "Begin Hour",
+  wafMethodCode: "WAF Method",
+  wafValue: "WAF Value",
+  numberOfTestRuns: "Number of Test Runs",
+  numberOfTraversePointsWAF: "Number of Traverse Points WAF",
+  numberOfTestPorts: "Number of Test Ports",
+  numberOfTraversePointsRef: "Number of Traverse Points Reference",
+  ductWidth: "Duct Width",
+  ductDepth: "Duct Depth",
+  wafEndDate: "End Date",
+  wafEndHour: "End Hour"
+};
+
+function replaceStringValues(inputString) {
+  let outputString = inputString;
+
+  for (const [key, value] of Object.entries(replacements)) {
+    const regex = new RegExp(key, 'g'); 
+    outputString = outputString.replace(regex, value); 
+  }
+
+  return outputString;
+}
+
 export function parseErrorMessage(error) {
   let errorMessage = "";
 
@@ -41,7 +69,7 @@ export function parseErrorMessage(error) {
       status: error.response.status,
       headers: error.response.headers,
     });
-    errorMessage = error.response.data?.message;
+    errorMessage = replaceStringValues(error?.response?.data?.message)?.replaceAll(null, ' ');
   } else if (error.request) {
     // client never received a response, or request never left
     log.error({ error: error.request });
