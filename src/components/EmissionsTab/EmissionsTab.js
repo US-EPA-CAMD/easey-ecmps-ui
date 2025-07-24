cfeee7fb4a7f684444da654367f89061f0783dfd
Jon Maxwell Diebold <max.diebold@erg.com>
2025-07-24 13:53:17 -0400

    fix: location select state issue on Emissions tabs

--- src/components/EmissionsTab/EmissionsTab.js
+++ src/components/EmissionsTab/EmissionsTab.js
@@ -27,11 +27,8 @@ export const EmissionsTab = ({
   };
 
   const currentTab = getCurrentTab();
-  const [locationSelect, setLocationSelect] = useState(currentTab.location);
-
-  useEffect(() => {
-    setLocation(locationSelect, title, EMISSIONS_STORE_NAME);
-  }, [locationSelect, setLocation, title]);
+  const locationSelect = currentTab?.location ?? null;
+  const setLocationSelect = (location) => setLocation(location, title, EMISSIONS_STORE_NAME);
 
   return (
     <EmissionsTabRender
